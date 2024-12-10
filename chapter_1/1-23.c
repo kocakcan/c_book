4746e70517400aef7a01cce6a88a706f989c8e1e
cankocak <can_kocak1@epam.com>
2024-12-10 10:26:15 +0300

    added 1-23

---
+++ chapter_1/1-23.c
@@ -0,0 +1,11 @@
+/*   Write a program to remove all comments from a C program. Don't forget to
+ *   handle quoted strings and character constants properly. C comments don't
+ *   nest.
+ */
+#include <stdio.h>
+
+int main() {
+  printf("aye, siwmae\n");
+
+  return 0;
+}
