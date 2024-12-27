98efc0233d42aa43e48456e728dddf7533e585f9
cankocak <can_kocak1@epam.com>
2024-12-27 15:16:40 +0300

    completed 2-1

--- chapter_2/2-1.c
+++ chapter_2/2-1.c
@@ -3,7 +3,6 @@
  *  standard headers and by direct computation. Harder if you compute them:
  *  determine the ranges of the various floating-point types.
  */
-#include <float.h>
 #include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -16,18 +15,33 @@ int main() {
   // In two's complement representation, zero is considered part of the
   // positive range, which is why it is subtracted from the upper limit but not
   // from the lower limit.
+
+  // short and long are type qualifiers applied to integers.
   printf("On this machine:\n");
   printf("int: %ld bits\n", (sizeof(int) * 8));
   printf("INT_MIN: %d\n", INT_MIN);
   printf("INT_MAX: %d\n", INT_MAX);
   printf("signed int: %ld bits\n", (sizeof(signed int) * 8));
   printf("unsigned int: %ld bits\n", (sizeof(unsigned int) * 8));
-  printf("UINT_MAX: %d\n", UINT_MAX);
+  printf("UINT_MAX: %ud\n", UINT_MAX);
   printf("short: %ld bits\n", (sizeof(short) * 8));
+  printf("signed short: %ld bits\n", (sizeof(signed short) * 8));
+  printf("unsigned short: %ld bits\n", (sizeof(unsigned short) * 8));
   printf("USHRT_MAX: %d\n", USHRT_MAX);
+  printf("SHRT_MAX: %d\n", SHRT_MAX);
   printf("long: %ld bits\n", (sizeof(long) * 8));
+  printf("LONG_MAX: %ld\n", LONG_MAX);
   printf("unsigned long: %ld bits\n", (sizeof(unsigned long) * 8));
-  printf("ULONG_MAX: %ld\n", ULONG_MAX);
+  printf("signed long: %ld bits\n", (sizeof(signed long) * 8));
+  printf("ULONG_MAX: %lu\n", ULONG_MAX);
+
+  // chars are signed by default on this machine
+  printf("char: %lu bits\n", (sizeof(char) * 8));
+  printf("char: %d\n", CHAR_MAX);
+  printf("unsigned char: %d\n", UCHAR_MAX);
+  printf("short char: %d\n", CHAR_MAX);
+  printf("SCHAR_MAX: %d\n", SCHAR_MAX);
+  printf("SCHAR_MIN: %d\n", SCHAR_MIN);
 
   int nchars = printf("Today I've learned that printf returns the numbers of "
                       "characters successfully read. Wow!\n");
