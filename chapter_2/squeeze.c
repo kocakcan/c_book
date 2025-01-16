db81d5835608705944d0f4ca992d165d131c7bef
cankocak <can_kocak1@epam.com>
2025-01-16 18:41:24 +0300

    that's so logical

--- chapter_2/squeeze.c
+++ chapter_2/squeeze.c
@@ -6,27 +6,15 @@ enum bool { NO, YES };
 /* squeeze: delete all c from s */
 void squeeze(char s[], int c);
 
+/* strcat: concatenate t to the end of s; s must be big enough */
+void strcat_(char s[], char t[]);
+
 int main() {
   int my_bool = NO;
   enum bool bool_var = 5;
   printf("The value of bool_var is: %d\n", bool_var);
   printf("Sekiro is an amazing game mate\n");
   int flag = nil;
-  int nums[10] = {};
-  const int size = sizeof(nums) / sizeof(nums[0]);
-
-  for (int i = 0; i < 10; ++i) {
-    for (int k = 0; k < size; ++k) {
-      nums[k] = i * k;
-    }
-  }
-
-  printf("Here's the array: \n");
-
-  for (int i = 0; i < size; ++i)
-    printf("%d ", nums[i]);
-
-  printf("\n");
 
   if (!flag)
     printf("This is the worst TV show ever mate.\n");
@@ -36,6 +24,13 @@ int main() {
   printf("Here's my name before: %s\n", name);
   squeeze(name, 'a');
   printf("Here's my name after: %s\n", name);
+
+  char s[10];
+  char t[] = "Kocak";
+
+  strcat_(s, t);
+
+  printf("Here's my surname: %s\n", s);
 
   return 0;
 }
@@ -50,4 +45,14 @@ void squeeze(char s[], int c) {
   }
 
   s[j] = '\0';
+}
+
+void strcat_(char s[], char t[]) {
+  int i = 0, j = 0;
+
+  while (s[i] != '\0')
+    ++i;
+
+  while ((s[i++] = t[j++]) != '\0')
+    ;
 }
