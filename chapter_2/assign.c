77906b2ac0c67d1f2c2937e204611ff710d26ec1
cankocak <can_kocak1@epam.com>
2025-02-05 06:26:39 +0300

    understood how binary representation works

--- chapter_2/assign.c
+++ chapter_2/assign.c
@@ -11,6 +11,9 @@
 //   statement or an expression depending on whether the function returns a
 //   value
 
+/* bitcount: count 1 bits in x */
+int bitcount(unsigned x);
+
 int main(void) {
   int x = 5;
 
@@ -55,4 +58,41 @@ int main(void) {
   if (z > 5) { // Statement: conditional (where z > 5 is an expression)
     printf("z is greater than 5\n");
   }
+
+  unsigned t = 255;
+
+  if (bitcount(t) == 8) {
+    printf("That's where you're right kiddo!\n");
+  }
+  // Since this is a signed int, it will have its leftmost bit 1
+  // 00001111 is the positive 15
+  // Inverting its bits -> 11110000
+  // Add +1 to it -> 11110001
+  // 11110001 = -15 in decimal
+  // Vacated bits are filled with bit signs
+  // Right shift once: 11111000 = -8 in decimal
+  // Right shift twice: 11111100 = -4 in decimal
+  int u = -15;
+
+  printf("u is %d\n", u);
+
+  // 11110001
+  // 11111100 = -4
+  u = u >> 2;
+
+  if (u == -4) {
+    printf("Yes, you got i! u is %d\n", u);
+  }
+}
+
+int bitcount(unsigned x) {
+  int b;
+
+  for (b = 0; x != 0; x >>= 1) {
+    if (x & 01) {
+      b++;
+    }
+  }
+
+  return b;
 }
