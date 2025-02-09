ef18984bbb00f1ad2dff22454bb34ab1dd19bc50
cankocak <can_kocak1@epam.com>
2025-02-09 11:40:52 +0300

    added 2-10

--- chapter_2/2-10.c
+++ chapter_2/2-10.c
@@ -2,8 +2,20 @@
  * with a conditional expression instead of if-else. */
 #include <stdio.h>
 
-int main(void) {
-  // TODO: Implement the function
+/* lower: convert c to lower case; ASCII only */
+int lower(int c) {
+  if (c >= 'A' && c <= 'Z')
+    return c + 'a' - 'A';
+  else
+    return c;
+}
 
-  printf("aye, siwmae\n");
+/* lower_: convert c to lower case; ASCII only, using conditional expression */
+int lower_(int c) { return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c; }
+
+int main(void) {
+  char c = 'A';
+
+  if ('a' == lower_(c))
+    printf("That's correct mate!\n");
 }
