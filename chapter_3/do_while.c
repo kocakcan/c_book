4101f8903d7a6e54bb4f81eeb94a16ddaecafda0
cankocak <can_kocak1@epam.com>
2025-04-09 21:37:34 +0300

    completed do-while

---
+++ chapter_3/do_while.c
@@ -0,0 +1,65 @@
+// The while and for loops test the termination condition at the top. By
+// contrast, the third loop in C, the do-while, tests at the bottom after
+// making each pass through the loop body, the body is always executed at least
+// once.
+
+/***
+ *   The syntax of the do is
+ *   do
+ *     statement
+ *   while (expression);
+ *
+ *   - The statement is executed, then expression is evaluated, If it is true,
+ *   statement is evaluated again, and so on. When the expression becomes false,
+ *   the loop terminates. Except for the sense of the test, do-while is
+ *   equivalent to Pascal repeat-until statement.
+ *
+ *   - Experience shows that do-while is much less used than while and for.
+ *     Nonetheless, from time to time it is valuable, as in the following
+ *     function itoa, which converts a number to a character string (the inverse
+ *     of atoi).
+ */
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char s[]) {
+  int i, j, c;
+
+  for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
+
+/* itoa: convert n to characters in s */
+void itoa(int n, char s[]) {
+  int i, sign;
+
+  if ((sign = n) < 0) /* record sign */
+    n = -n;           /* make n positive */
+
+  i = 0;
+
+  do {                     /* generate digits in reverse order */
+    s[i++] = n % 10 + '0'; /* get next digit */
+  } while ((n /= 10) > 0); /* delete it */
+
+  if (sign > 0) {
+    s[i++] = '-';
+  }
+
+  s[i] = '\0';
+  reverse(s);
+}
+
+int main() {
+  int n = 123;
+  char s[] = "1997";
+
+  itoa(n, s);
+
+  printf("%s\n", s);
+
+  return 0;
+}
