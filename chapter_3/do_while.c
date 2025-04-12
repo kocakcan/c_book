484815d35d27868fd2f1b55e28c521ebb1e6aa67
Can <106063192+kocakCan@users.noreply.github.com>
2025-04-12 11:06:42 +0300

    Update do_while.c

--- chapter_3/do_while.c
+++ chapter_3/do_while.c
@@ -21,6 +21,7 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 void reverse(char s[]) {
   int i, j, c;
@@ -35,15 +36,16 @@ void reverse(char s[]) {
 /* itoa: convert n to characters in s */
 void itoa(int n, char s[]) {
   int i, sign;
+  unsigned long num;
 
-  if ((sign = n) < 0) /* record sign */
-    n = -n;           /* make n positive */
+  if ((sign = n) < 0)               /* record sign */
+    num = (unsigned long)(~n + 1);  /* make it unsigned to avoid UB */
 
   i = 0;
 
-  do {                     /* generate digits in reverse order */
-    s[i++] = n % 10 + '0'; /* get next digit */
-  } while ((n /= 10) > 0); /* delete it */
+  do {                             /* generate digits in reverse order */
+    s[i++] = n % 10 + '0';         /* get next digit */
+  } while ((n /= 10) > 0);         /* delete it */
 
   if (sign < 0) {
     s[i++] = '-';
@@ -54,7 +56,7 @@ void itoa(int n, char s[]) {
 }
 
 int main() {
-  int n = -123;
+  int n = INT_MIN;
   char s[100];
 
   itoa(n, s);
