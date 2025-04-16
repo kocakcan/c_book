f7d71cca3159af31cd9833bd688cb2c81cb79914
cankocak <can_kocak1@epam.com>
2025-04-16 19:27:38 +0300

    such a beautiful solution

--- chapter_3/3-5.c
+++ chapter_3/3-5.c
@@ -1,11 +1,46 @@
 /* Write the function itob(n, s, b) that converts the integer n into a base b character representation in the string s. In particular, itob(n, s, 16) formats s as a hexadecimal integer in s. */
 #include <stdio.h>
+#include <string.h>
 
-// TODO: Implement this function
 void itob(int n, char s[], int b);
+void reverse(char s[]);
 
 int main(int argc, char *argv[]) {
-	printf("Oi oi Hughie!\n");
+	int n = 0xabcff;
+	char s[100];
+
+	itob(n, s, 8);
+	printf("%s\n", s);
 
 	return 0;
+}
+
+void itob(int n, char s[], int b) {
+	int i, sign;
+	char digits[] = "0123456789abcdef";
+
+	if ((sign = n) < 0)
+		n = -n;
+
+	i = 0;
+
+	do {
+		s[i++] = digits[n % b];
+	} while ((n /= b) != 0);
+
+	if (sign < 0)
+		s[i++] = '-';
+
+	s[i] = '\0';
+	reverse(s);
+}
+
+void reverse(char s[]) {
+	int i, j, c;
+
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
 }
