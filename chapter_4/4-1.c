b5761cf1930e82a7052da918cf55d1e7cb4c9888
cankocak <can_kocak1@epam.com>
2025-04-20 18:36:28 +0300

    added 4-1

---
+++ chapter_4/4-1.c
@@ -0,0 +1,10 @@
+/* Write the function strindex(s, t) which returns the position of the rightmost occurrence of t in s, or -1 if there is none. */
+#include <stdio.h>
+
+// TODO: Implement this function
+int strindex(char s[], char t[]);
+
+int main() {
+
+	return 0;
+}
