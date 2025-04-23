0b1d81604596111e704af8649130df005215a400
cankocak <can_kocak1@epam.com>
2025-04-23 09:17:31 +0300

    added comment for the inner loop

--- chapter_4/4-1.c
+++ chapter_4/4-1.c
@@ -1,7 +1,8 @@
-/* Write the function strindex(s, t) which returns the position of the rightmost occurrence of t in s, or -1 if there is none. */
+/* Write the function strindex(s, t) which returns the position of the rightmost
+ * occurrence of t in s, or -1 if there is none. */
 #include <stdio.h>
 #include <string.h>
-#define MAXLINE	1000
+#define MAXLINE 1000
 
 /* strindex: return index of the rightmost occurrence of t in s, -1 if none */
 int strindex(char s[], char t[]);
@@ -10,40 +11,41 @@ int getline_(char s[], int lim);
 char pattern[] = "lo";
 
 int main() {
-	char line[MAXLINE];
-	int found = 0;
+  char line[MAXLINE];
+  int found = 0;
 
-	while (getline_(line, MAXLINE) > 0) {
-		if (strindex(line, pattern) >= 0) {
-			printf("%s", line);
-			found++;
-		}
-	}
-	return found;
+  while (getline_(line, MAXLINE) > 0) {
+    if (strindex(line, pattern) >= 0) {
+      printf("%s", line);
+      found++;
+    }
+  }
+  return found;
 }
 
 int strindex(char s[], char t[]) {
-	int i, j, k;
+  int i, j, k;
 
-	for (i = strlen(s) - 1; i >= 0; i--) {
-		for (j = i, k = strlen(t) - 1; j >= 0 && s[j] == t[k]; j--, k--)
-			;
-		if (k < 0)
-			return j + 1;
-	}
-	return -1;
+  for (i = strlen(s) - 1; i >= 0; i--) {
+    for (j = i, k = strlen(t) - 1; j >= 0 && s[j] == t[k]; j--, k--)
+      ;
+    // k should end up being negative as its traversed in reverse
+    if (k < 0)
+      return j + 1;
+  }
+  return -1;
 }
 
 int getline_(char s[], int lim) {
-	int i, c;
+  int i, c;
 
-	i = 0;
-	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
-		s[i++] = c;
+  i = 0;
+  while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+    s[i++] = c;
 
-	if (c == '\n')
-		s[i++] = c;
+  if (c == '\n')
+    s[i++] = c;
 
-	s[i] = '\0';
-	return i;
+  s[i] = '\0';
+  return i;
 }
