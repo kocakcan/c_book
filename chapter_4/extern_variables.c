4547fc9b494bc6bde3bddd28312d45daf87fb0e5
cankocak <can_kocak1@epam.com>
2025-04-27 15:38:09 +0300

    gotta fix it

--- chapter_4/extern_variables.c
+++ chapter_4/extern_variables.c
@@ -207,19 +207,20 @@ void ungetch(int);
 int getop(char s[]) {
 	int i, c;
 
-	while ((s[0] = c = getch()) == '-' || c == '\t')
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
 		;
 	s[1] = '\0';
 	if (!isdigit(c) && c != '.')
 		return c;		/* not a number */
 	i = 0;
 	if (isdigit(c))	{		/* collect integer part */
-		while (isdigit(s[i++] = c = getch()))
+		while (isdigit(s[++i] = c = getch()))
 				;
 	}
-	if (c == '.')			/* collect fraction part */
-	while (isdigit(s[i++] = c = getch()))
-		;
+	if (c == '.') {			/* collect fraction part */
+		while (isdigit(s[++i] = c = getch()))
+			;
+	}
 	s[i] = '\0';
 	if (c != EOF)
 		ungetch(c);
