ec52933e270eb7907de2f54c9d616dcd655d3510
cankocak <can_kocak1@epam.com>
2025-05-09 18:37:23 +0300

    completed 4-9

--- chapter_4/4-9.c
+++ chapter_4/4-9.c
@@ -1,3 +1,40 @@
 /* Our getch and ungetch do not handle a pushed-back EOF correctly. Decide what
  * their properties ought to be if an EOF is pushed back, then implement your
  * design. */
+#include <stdio.h>
+
+char c = 0;
+
+int getch(void) {
+  int temp = c;
+  c = 0;
+
+  // return (temp) ? temp : getchar();
+  return (temp) ? temp : getchar();
+}
+
+void ungetch(int ch) {
+  if (c)
+    printf("ungetch: already read a character\n");
+  else
+    c = ch;
+}
+
+int main(void) {
+  // int to accommodate EOF
+  int ch;
+
+  printf("Enter a character: ");
+  ch = getch();
+  printf("You've entered: '%c' (ASCII: %d)\n", (char)ch, ch);
+
+  ungetch(ch);
+  ch = getch();
+  printf("After ungetch: '%c' (ASCII: %d)\n", (char)ch, ch);
+
+  ungetch(EOF);
+  ch = getch();
+  if (ch == EOF) {
+    printf("After pushing back EOF, received EOF again.\n");
+  }
+}
