433404f21f64248b8429e385f99daabe1f850f6b
cankocak <can_kocak1@epam.com>
2025-05-11 21:12:51 +0300

    completed 4-10

--- chapter_4/4-10.c
+++ chapter_4/4-10.c
@@ -3,162 +3,151 @@
  * approach. */
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#define MAXOP	100
-#define NUMBER	'0'
-#define BUFSIZE	100
+#define MAXOP   100    // Max size of operand or operator
+#define NUMBER  '0'    // Signal that a number was found
+#define BUFSIZE 100    // Input buffer size
+#define MAXVAL  100    // Maximum depth of val stack
 
-char buf[BUFSIZE];
-int bufp = 0;
+char buf[BUFSIZE];     // Input line buffer
+int buf_index = 0;     // Current position in buf
 
+// Stack
+int sp = 0;             // Next free stack position
+double val[MAXVAL];     // Value stack
+
+// Function declarations
 int getop(char []);
 void push(double);
 double pop(void);
 int getline_(void);
 
-int main() {
-	int type;
-	double op2;
-	char s[MAXOP];
+int main(void) {
+    int type;
+    double op1, op2;
+    char s[MAXOP];
 
-	while (getline_() > 0) {
-		// Adjust the index for null terminator
-		bufp--;
-		while ((type = getop(s)) != EOF) {
-			switch (type) {
-			case NUMBER:
-				push(atof(s));
-				break;
-			case '+':
-				push(pop() + pop());
-				break;
-			case '*':
-				push(pop() * pop());
-				break;
-			case '-':
-				op2 = pop();
-				push(pop() - op2);
-				break;
-			case '/':
-				op2 = pop();
-				if (op2 != 0.0)
-					push(pop() / op2);
-				else
-					printf("error: zero divisor\n");
-				break;
-			case '%':
-				op2 = pop();
-				double op1 = pop();
-				if ((int)op2 != 0)
-					push((int)op1 % (int)op2);
-				else
-					printf("error: zero divisor for modulus\n");
-				break;
-			case '\n':
-				printf("\t%.8g\n", pop());
-				break;
-			default:
-				printf("error: unknown command %s\n", s);
-				break;
-			}
-		}
-	}
-	return 0;
+    while (getline_() > 0) {
+        buf_index = 0;  // Reset input buffer position at start of line
+
+        while ((type = getop(s)) != '\0') {
+            switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '%':
+                op2 = pop();
+                op1 = pop();
+                if ((int)op2 != 0)
+                    push((int)op1 % (int)op2);
+                else
+                    printf("error: zero divisor for modulus\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+            }
+        }
+    }
+
+    return 0;
 }
 
-#define MAXVAL	100
-
-int sp = 0;
-double val[MAXVAL];
-
 void push(double f) {
-	if (sp < MAXVAL)
-		val[sp++] = f;
-	else
-		printf("error: stack full, can't push %g\n", f);
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
 }
 
 double pop(void) {
-	if (sp > 0)
-		return val[--sp];
-	else {
-		printf("error: stack empty\n");
-		return 0.0;
-	}
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
 }
 
-#include <ctype.h>
-
-int getch(void);
-void ungetch(int);
-
+// Reads next operator or numeric operand into s
 int getop(char s[]) {
-	int i = 0, c, next;
+    int i = 0;
+    int c;
 
-	while ((s[0] = c = buf[--bufp]) == ' ' || c == '\t')
-		;
-	s[1] = '\0';
-	if (!isdigit(c) && c != '.' && c != '-')
-		return c;
-	if (c == '-') {
-		next = buf[--bufp];
-		if (!isdigit(next) && next != '.') {
-			buf[bufp++] = next;	// not a negative number
-			return c;		// return '-' operator
-		}
-		s[++i] = c = next;
-	}
-	if (isdigit(c)) {
-		while (isdigit(s[++i] = c = buf[--bufp]))
-			;
-	}
-	if (c == '.') {
-		while (isdigit(s[++i] = c = buf[--bufp]))
-			;
-	}
-	s[i] = '\0';
-	if (c != EOF)
-		buf[bufp++] = c;
-	return NUMBER;
+    // Skip whitespace
+    while ((c = buf[buf_index++]) == ' ' || c == '\t')
+        ;
+
+    if (c == '\0')
+        return '\0';
+
+    s[0] = c;
+    s[1] = '\0';
+
+    // Not a number
+    if (!isdigit(c) && c != '.' && c != '-')
+        return c;
+
+    // Handle negative numbers
+    if (c == '-') {
+        int next = buf[buf_index];
+        if (!isdigit(next) && next != '.') {
+            return '-';  // Minus operator
+        }
+        c = buf[buf_index++];
+        s[++i] = c;
+    }
+
+    // Integer part
+    while (isdigit(c = buf[buf_index++]))
+        s[++i] = c;
+
+    // Fractional part
+    if (c == '.') {
+        s[++i] = c;
+        while (isdigit(c = buf[buf_index++]))
+            s[++i] = c;
+    }
+
+    s[++i] = '\0';
+
+    if (c != '\0')
+        buf_index--;  // Unread one character
+
+    return NUMBER;
 }
 
-// #define BUFSIZE	100
-
-// char buf[BUFSIZE];
-// int bufp = 0;
-
-// int getch(void) {
-// 	int temp = (bufp > 0) ? buf[--bufp] : getchar();
-// 	buf[--bufp] = 0;
-//
-// 	return temp;
-// }
-//
-// void ungetch(int ch) {
-// 	if (buf[--bufp])
-// 		printf("ungetch: too many characters\n");
-// 	else
-// 		buf[bufp++] = c;
-// }	
-// int getch(void) {
-// 	return (bufp > 0) ? buf[--bufp] : getchar();
-// }
-//
-// void ungetch(int c) {
-// 	if (bufp >= BUFSIZE)
-// 		printf("ungetch: too many characters\n");
-// 	else
-// 		buf[bufp++] = c;
-// }
+// Reads a line of input into buf
 int getline_() {
-	int c;
-	int lim = BUFSIZE;
-	bufp = 0;
+    int c, i = 0;
 
-	while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
-		buf[bufp++] = c;
-	if (c == '\n')
-		buf[bufp++] = c;
-	buf[bufp] = '\0';
-	return bufp;
+    while ((c = getchar()) != EOF && c != '\n' && i < BUFSIZE - 1)
+        buf[i++] = c;
+
+    if (c == '\n')
+        buf[i++] = c;
+
+    buf[i] = '\0';
+    return i;
 }
