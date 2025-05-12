ed159530a0dcfbd44dfe22dfa8233c54b864c746
cankocak <can_kocak1@epam.com>
2025-05-12 19:42:30 +0300

    started 4.5 Header Files

---
+++ chapter_4/header_files.c
@@ -0,0 +1,13 @@
+/***
+ * 4.5 Header Files
+ *
+ * Let us now consider dividing the calculator program into several source
+ * files, as it might be each of the components were substantially bigger. The
+ * main function would go in one file, which we will call main.c; push, pop, and
+ * their variables go into a second file, stack.c; getop goes into a third,
+ * getop.c. Finally, getch and ungetch go into a fourth file, getch.c; we
+ * seperate them from the others because they would come from a
+ * seperately-compiled library in a realistic program.
+ *
+ * TODO: Continue from this point
+ */
