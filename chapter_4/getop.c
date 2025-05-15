75baecd5784136d6403aa5ba74d0bd43a523146f
cankocak <can_kocak1@epam.com>
2025-05-15 17:05:00 +0300

    now it works properly 😎

--- chapter_4/getop.c
+++ chapter_4/getop.c
@@ -1,61 +1,24 @@
 #include "calc.h"
 #include <ctype.h>
-#include <stdio.h>
-
-// int getop(char s[]) {
-//   int i = 0, c, next;
-//
-//   while ((s[0] = c = getch()) == ' ' || c == '\t')
-//     ;
-//
-//   s[1] = '\0';
-//   if (!isdigit(c) && c != '.' && c != '-')
-//     return c;
-//   if (c == '-') {
-//     next = getch();
-//     if (!isdigit(next) && next != '.') {
-//       ungetch(next);
-//       return c;
-//     }
-//     s[++i] = c = next;
-//   }
-//   if (isdigit(c)) {
-//     while (isdigit(s[++i] = c = getch()))
-//       ;
-//   }
-//   if (c == '.') {
-//     while (isdigit(s[++i] = c = getch()))
-//       ;
-//   }
-//   s[i] = '\0';
-//   if (c != EOF)
-//     ungetch(c);
-//   return NUMBER;
-// }
 
 int getop(char s[]) {
-  static int last_ch = 0;
   int i = 0;
   int c;
+  static int last_ch = 0;
 
-  c = (last_ch != 0) ? last_ch : getch();
+  c = (last_ch) ? last_ch : getch();
   last_ch = 0;
 
-  while (s[0] == ' ' || s[0] == '\t') {
-    s[0] = c;
+  while (c == ' ' || c == '\t')
     c = getch();
-  }
 
-  s[1] = '\0';
-  if (!isdigit(c) && c != '.' && c != '-') {
+  if (!isdigit(c) && c != '.' && c != '-')
     return c;
-  }
 
   if (c == '-') {
     last_ch = getch();
-    if (!isdigit(last_ch) && last_ch != '.') {
+    if (!isdigit(last_ch) && last_ch != '.')
       return '-';
-    }
     s[i++] = '-';
     c = last_ch;
     last_ch = 0;
@@ -72,6 +35,7 @@ int getop(char s[]) {
     while (isdigit(s[i++] = c = getch()))
       ;
   }
+
   s[i] = '\0';
   return NUMBER;
 }
