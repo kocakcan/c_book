7efd77ad250507d3800bb887d4964ca7b0c8dea3
cankocak <can_kocak1@epam.com>
2025-05-21 17:49:53 +0300

    added swap(), still needs some work

--- chapter_4/4-13.c
+++ chapter_4/4-13.c
@@ -5,9 +5,17 @@
 
 void reverse(char s[]) {
   int i, j, c;
+  void swap(char s[], char i, char j);
 
   for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
-    c = s[i], s[i] = s[j], s[j] = c;
+    swap(s, i, j);
+  // c = s[i], s[i] = s[j], s[j] = c;
+}
+
+void swap(char s[], char i, char j) {
+  int temp;
+
+  temp = s[i], s[i] = s[j], s[j] = temp;
 }
 
 int main(void) {
