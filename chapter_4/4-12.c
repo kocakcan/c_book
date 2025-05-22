2446fec4c441b5b1dd7ae1d4d2430551cff333a3
cankocak <can_kocak1@epam.com>
2025-05-22 17:52:52 +0300

    fixed it also handled UB

--- chapter_4/4-12.c
+++ chapter_4/4-12.c
@@ -1,34 +1,42 @@
 /* Adapt the ideas of printd to write a recursive version of itoa; that is, convert an integer into a string by calling a recursive routine. */
 #include <stdio.h>
+#include <limits.h>
 
-void itoa(int n, char s[]) {
-	// Need not initialize i as it is static
-	// it will be initialized to zero by default
-	static int i;
-	int sign;
-
-	if ((sign = n) < 0)
-		n = -n;
-
-	if (sign < 0)
-		s[i++] = '-';
-
-	// do {
-		// s[i++] = n % 10 + '0';
-	// } while ((n /= 10) > 0);
-	
+int utoa(unsigned int n, char s[], int i) {
 	if (n / 10)
-		itoa(n / 10, s);
-	s[i++] = n % 10 + '0';
+		i = utoa(n / 10, s, i);
 
+	s[i++] = n % 10 + '0';
 	s[i] = '\0';
-	// reverse(s);
+	return i;
+}
+
+int itoa(int n, char s[], int i) {
+	if (n < 0) {
+		s[i++] = '-';
+		return utoa((unsigned int)(~n + 1), s, i);
+	}
+
+	return utoa((unsigned int)n, s, i);
+}
+
+void itoa_wrapper(int n, char s[]) {
+	itoa(n, s, 0);
 }
 
 int main(void) {
-	char s[10];
-	int n = -1965;
-	itoa(n, s);
+	char s[20];
+	itoa_wrapper(INT_MIN, s);
+	int x = 1530, y = -3150;
+	printf("%s\n", s);
+
+	itoa_wrapper(INT_MAX, s);
+	printf("%s\n", s);
+
+	itoa_wrapper(x, s);
+	printf("%s\n", s);
+
+	itoa_wrapper(y, s);
 	printf("%s\n", s);
 
 	return 0;
