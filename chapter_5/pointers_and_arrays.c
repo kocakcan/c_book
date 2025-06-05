c0ce6e49c732571505ca807885f74d3994c13022
cankocak <can_kocak1@epam.com>
2025-06-05 18:18:49 +0300

    completed 5.3

--- chapter_5/pointers_and_arrays.c
+++ chapter_5/pointers_and_arrays.c
@@ -51,5 +51,77 @@
  * 	pa = a;
  * Rather more surprising, at first sight, is the fact that a reference to a
  * [i] can also be written as *(a+i). In evaluating a[i], C converts it to
- * *(a+i) immediately; the two forms are equivalent. Applying the operator
- */
+ * *(a+i) immediately; the two forms are equivalent. Applying the operator & to
+ * both parts of this equivalence, it follows that &a[i] and a+i are also
+ * identical: a+i is the address of the i-th element beyond a. As the other
+ * side of this coin, if pa is a pointer, expression might use it with a
+ * subscript; pa[i] is identical to *(pa+i). In short, an array-and-index
+ * expression is equivalent to one written as a pointer and offset.
+ *
+ * There is one difference between an array name and a pointer that must be
+ * kept in mind. A pointer is a variable, so pa=a and pa++ are legal. But an
+ * array name is not a variable; constructions like a=pa and a++ are illegal.
+ * When an array name is passed to a function, what is passed is the location
+ * of the initial element. Within the called function, this argument is a local
+ * variable, and so an array name parameter is a pointer, that is, a variable
+ * containing address. We can use this fact to write another version of strlen,
+ * which computers the length of a string.
+ *
+ * Since s is a pointer, incrementing it is perfectly legal; s++ has no effect
+ * on the character string in the function that called strlen, but merely
+* increments strlen'ss private copy of the pointer. That means that calls like
+*
+* 	strlen("hello, world");	-> string constant
+* 	strlen(array);		-> char array[100];
+* 	strlen(ptr);		-> char *ptr;
+* all work.
+*
+* As formal parameters in a function declaration,
+*
+*	char s[];
+* and
+*
+* 	char *s;
+* are equivalent; the latter is preferred because it says more explicitly that
+* a variable is a pointer. When an array name is passed to a function, the
+* function can at its convenience believe that it has been handed either an
+* array or a pointer, and manipulate it accordingly. It can even use both
+* notations if it seems appropriate and clear.
+*
+* It is possible to pass part of an array to a function, by passing a pointer
+* to the beginning of the subarray. For example, if a is an array,
+*
+* 	f(&a[2])
+* and
+* 	f(a+2)
+* both pass to the function f the address of the subarray that starts at a[2].
+* Within f, the parameter declaration can read
+*
+* 	f(int arr[]) { ... }
+* or
+* 	f(int *arr) { ... }
+* So as far as f is concerned, the fact that parameter refers to part of a
+* larger array is of no consequence.
+*
+* If one is sure that the element exist, it is also possible to index backwards
+* in an array; p[-1], p[-2], and so on are syntactically legal, and refer to
+* the elements that immediately precede p[0]. Of course, it is illegal to refer
+* to objects that are not within the array bounds.
+*/
+#include <stdio.h>
+
+int strlen_(char *s) {
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+		i++;
+
+	return i;
+}
+
+int main(void) {
+	char *s = "Seyfi Kocak";
+	printf("%s is %d characters long\n", s, strlen_(s));
+
+	return 0;
+}
