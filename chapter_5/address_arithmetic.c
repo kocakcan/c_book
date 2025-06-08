dcd8fdbbc6f68d8c84bcb3b1307ce2e4887fcbfd
cankocak <can_kocak1@epam.com>
2025-06-08 20:05:26 +0300

    fixed a typo

--- chapter_5/address_arithmetic.c
+++ chapter_5/address_arithmetic.c
@@ -3,7 +3,7 @@
  *
  * If p is a pointer to some element of an array, then p++ increments p to
  * point to the next element, and p += i increments it to point i elements
- * beyond where it currently does. These an similar constructions are the
+ * beyond where it currently does. These similar constructions are the
  * simplest forms of pointer or address arithmetic.
  *
  * C is consistent and regular in its approach to address arithmetic; its
