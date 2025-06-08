5e442028a90aa5c82591ff0e89f2f779c288d41a
cankocak <can_kocak1@epam.com>
2025-06-08 18:33:53 +0300

    continue working on it

--- chapter_5/address_arithmetic.c
+++ chapter_5/address_arithmetic.c
@@ -27,8 +27,58 @@
  * instead be obtained by calling malloc or by asking the operating system for
  * a pointer to some unnamed block of storage.
  *
+ * The other information needed is how much of allocbuf has been used. We use a
+ * pointer, calloc allocp, that points to the next free element. When alloc is
+ * asked for n characters, it checks to see if there is enough room left in
+ * allocbuf. If so, alloc returns the current value of allocp (i.e., the
+ * beginning of the free block), then increments it by n to point to the next
+ * free area. If there is no room, alloc returns zero. afree(p) merely sets
+ * allocp to p if p is inside allocbuf.
+ *
+ * In general a pointer can be initialized just as any other variable can,
+ * though normally the only meaningful values are zero or an expression
+ * involving the address of previously defined data of appropriate type. The
+ * declaration
+ *
+ * 	static char *allocp = allocbuf;
+ * defines allocp to be a character pointer and initializes it to point to the
+ * beginning of allocbuf, which is the next free position when the program
+ * starts. This could also have been written
+ *
+ * 	static char *allocp = &allocbuf[0];
+ * since the array name is the address of the zeroth element.
+ *
+ * The test
+ *
+ * 	if (allocbuf + ALLOCSIZE - allocp >= n)
+ * checks if there's enough room to satisfy a request for n characters. If
+ * there is, the new value of allocp would be at most one beyond the end of
+ * allocbuf. If the request can be satisfied, alloc returns a pointer to the
+ * beginning of a block of characters (notice the declaration of the function
+ * itself). If not, alloc must return some signal that there is no space left.
+ * C guarantees that zero is never a valid address for data, so a return value
+ * of zero can be used to signal an abnormal event, in this case no space.
  */
 #include <stdio.h>
+#define ALLOCSIZE	10000		/* size of available space */
+
+static char allocbuf(ALLOCSIZE);	/* storage for alloc */
+static char *allocp = allocbuf;		/* next free position */
+
+char *alloc(int n)			/* return pointer to n characters */{
+	if (allocbuf + ALLOCSIZE - allocp >= n) {	/* if it fits */
+		allocp += n;
+		return allocp - n;			/* old p */
+	} else {
+		return 0;
+	}
+}
+
+void afree(char *p)			/* free storage pointed to by p */
+{
+	if (p >= allocbuf && p < allocbuc + ALLOCSIZE)
+		allocp = p;
+}
 
 int main(void) {
 	int arr[] = {1, 3, 5, 7, 9, 11, 13, 15};
