90f114fa5d6697c05dd6c89de8c6ad08546769d9
cankocak <can_kocak1@epam.com>
2025-06-08 18:06:36 +0300

    started 5.4 Address Arithmetic

---
+++ chapter_5/address_arithmetic.c
@@ -0,0 +1,8 @@
+/***
+ * 5.4 Address Arithmetic
+ *
+ * If p is a pointer to some element of an array, then p++ increments p to
+ * point to the next element, and p += i increments it to point i elements
+ * beyond where it currently does. These an similar constructions are the
+ * simplest forms of pointer or address arithmetic.
+ */
