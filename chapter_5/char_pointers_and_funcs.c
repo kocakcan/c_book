ec5b3174804fe9fe5270879d39de0e8da987bc43
cankocak <can_kocak1@epam.com>
2025-06-11 16:12:58 +0300

    continue working on it

--- chapter_5/char_pointers_and_funcs.c
+++ chapter_5/char_pointers_and_funcs.c
@@ -1,3 +1,20 @@
 /***
  * 5.5 Character Pointers and Functions
+ *
+ * A string contans, written as
+ *
+ *  "I am a string"
+ * is an array of characters. In the internal representation, the array is
+ * terminated with the null character '\0' so that programs can find the end.
+ * The length in storage thus one more than the number of characters between
+ * the double quotes.
+ *
+ * Perhaps the most common occurrence of string contants is an arguments to
+ * functions, as in
+ *
+ *  printf("hello, world\n");
+ * When a character string like this appears in a program, access to it is
+ * through a character pointer; printf receives a pointer to the beginning of
+ * the character array. That is, a string constant is accessed by a pointer to
+ * its first element.
  */
