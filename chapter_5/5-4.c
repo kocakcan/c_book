441a770c5294df96561f5e9d1f48691ff4b494e3
cankocak <can_kocak1@epam.com>
2025-06-13 13:36:07 +0300

    added some pseudocode

--- chapter_5/5-4.c
+++ chapter_5/5-4.c
@@ -1,2 +1,7 @@
 /* Write the function strend(s, t), which returns 1 if the string t occurs at
  * the end of the string, and zero otherwise. */
+
+// while *s, while *t
+// traverse through each character array and locate their null terminators
+// traverse the character arrays again but in backwards this time
+// if you end up traversing the entire t then return 1, otherwise return 0
