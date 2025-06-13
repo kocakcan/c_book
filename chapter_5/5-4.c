5d0a5556c2943cda51e3bf73232b3933398bbabd
cankocak <can_kocak1@epam.com>
2025-06-13 14:30:47 +0300

    completed 5-4

--- chapter_5/5-4.c
+++ chapter_5/5-4.c
@@ -1,7 +1,41 @@
 /* Write the function strend(s, t), which returns 1 if the string t occurs at
- * the end of the string, and zero otherwise. */
+ * the end of the string s, and zero otherwise. */
+#include <stdio.h>
 
-// while *s, while *t
-// traverse through each character array and locate their null terminators
-// traverse the character arrays again but in backwards this time
-// if you end up traversing the entire t then return 1, otherwise return 0
+/* strend: return 1 if string t occurs at the end of string s, and 0 otherwise
+ * */
+int strend(char *s, char *t) {
+  int i, j;
+
+  j = 0;
+  while (*s) {
+    s++;
+    j++;
+  }
+
+  i = 0;
+  while (*t) {
+    t++;
+    i++;
+  }
+
+  if (j < i)
+    return 0;
+
+  for (; *s-- == *t--; i--) {
+    if (i == 0)
+      return 1;
+  }
+
+  return 0;
+}
+
+int main(void) {
+  char *s = "Knight Artorias";
+  char *t = "Artorias";
+
+  if (strend(s, t))
+    printf("%s appears at the end of %s\n", t, s);
+
+  return 0;
+}
