1bbc532f9cc154aba94903ded7797395a9afed4c
cankocak <can_kocak1@epam.com>
2025-06-14 20:21:23 +0300

    wrote the question for 5-5

---
+++ chapter_5/5-5.c
@@ -0,0 +1,3 @@
+/* Write versions of the library functions strncpy, strncat, and strcncmp,
+ * which operate on at most the first n characters of their argument strings.
+ * For example, strncpy(s, t, n) copies at most n characters of t to s. */
