34061a901d2f362fdab89da94d339a981f763a73
cankocak <can_kocak1@epam.com>
2025-06-17 19:00:53 +0300

    completed strncmp as well 😎

--- chapter_5/5-5.c
+++ chapter_5/5-5.c
@@ -21,11 +21,17 @@ void strncpy_(char *s, char *t, size_t n) {
     *s++ = '\0';     /* pad the rest of s with null */
 }
 
-int strcmp__(char *s, char *t) {
-  for (; *s == *t; s++, t++)
+int strncmp_(char *s, size_t n, char *t) {
+  size_t i;
+
+  for (i = 0; i < n && (*s || *t); i++, s++, t++) {
+    if (*s != *t)
+      return *s - *t;
     if (*s == '\0')
       return 0;
-  return *s - *t;
+  }
+
+  return (i == n) ? 0 : (*s - *t);
 }
 
 int main(void) {
@@ -34,13 +40,12 @@ int main(void) {
   char u[50];
   char v[] = "Knight Artorias, the Abysswalker";
   char x[] = "Can", y[] = "Can";
-  strcmp__(s, y);
   strncat_(s, t, 3);
   strncpy_(u, v, 55);
   printf("My full name is %s\n", s);
   printf("My favourite boss in Dark Souls is %s\n", u);
 
-  if (!strcmp__(x, y))
+  if (!strncmp_(x, 10, y))
     printf("they are the same string\n");
 
   return 0;
