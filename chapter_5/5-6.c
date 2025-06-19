785af68e3ff4bb2a603d9da826969edcbf08cfd5
cankocak <can_kocak1@epam.com>
2025-06-19 09:22:52 +0300

    added pointer version of strindex 👨‍💻

--- chapter_5/5-6.c
+++ chapter_5/5-6.c
@@ -3,6 +3,7 @@
  * itoa, and their variants, reverse, strindex and getop */
 #include <stdio.h>
 
+/* strindex: return index of t in s, -1 if none */
 int strindex(char *s, char *t) {
   int i, j, k;
 
@@ -16,10 +17,24 @@ int strindex(char *s, char *t) {
   return -1;
 }
 
+/* strindex: return index of t in s, -1 if none; pointer version */
+int strindex_(char *s, char *t) {
+  int i, j, k;
+
+  for (i = 0; *s; i++, s++) {
+    for (j = i, k = 0; *t && *s == *t; j++, k++, s++, t++)
+      ;
+    if (k > 0 && *t == '\0')
+      return i;
+  }
+
+  return -1;
+}
+
 int main(void) {
   char *s = "Can Kocak";
   char *t = "Kocak";
-  printf("%s is at index %d in %s\n", t, strindex(s, t), s);
+  printf("%s is at index %d in %s\n", t, strindex_(s, t), s);
 
   return 0;
 }
