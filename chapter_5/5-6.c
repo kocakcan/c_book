0d00c773c2666ea453ba7d54a3548f3d8b6041c8
cankocak <can_kocak1@epam.com>
2025-06-20 11:30:25 +0300

    added another version of getline

--- chapter_5/5-6.c
+++ chapter_5/5-6.c
@@ -39,6 +39,20 @@ int _getline(char s[], int lim) {
   return i;
 }
 
+int getline_(char s[], int lim) {
+  int c, i;
+
+  i = 0;
+  while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+    s[i++] = c;
+
+  if (c == '\n')
+    s[i++] = c;
+
+  s[i] = '\0';
+  return i;
+}
+
 int main(void) {
   char *s = "Can Kocak";
   char *t = "Kocak";
