388e482a223834726ec0ceb9ef643738406736fd
cankocak <can_kocak1@epam.com>
2025-06-30 20:25:51 +0300

    added 5.6 Pointer Arrays; Pointers to Pointers 🤓

--- chapter_5/pointer_arrays.c
+++ chapter_5/pointer_arrays.c
@@ -66,9 +66,13 @@ int main(void) {
 	}
 }
 
-#define MAXLEN	1000		/* max lenght of any input line */
-int getline(char *, int);
+#define MAXLEN		1000		/* max length of any input line */
+#define ALLOCSIZE	10000		/* size of available space */
+int getline_(char *, int);
 char *alloc(int);
+
+static char allocbuf[ALLOCSIZE];	/* storage for alloc */
+static char *allocp = allocbuf;		/* next free position */
 
 /* readlines: read input lines */
 int readlines(char *lineptr[], int maxlines) {
@@ -76,26 +80,29 @@ int readlines(char *lineptr[], int maxlines) {
 	char *p, line[MAXLEN];
 
 	nlines = 0;
-	while ((len = getline(line, MAXLEN)) > 0)
-		return - 1;
-	else {
-		line[len - 1] = '\0';	/*delete newline */
-		strcpy(p, line);
-		lineptr[nlines++] = p;
+	while ((len = getline_(line, MAXLEN)) > 0) {
+		if (nlines >= maxlines || (p = alloc(len)) == NULL)
+			return - 1;
+		else {
+			line[len - 1] = '\0';	/*delete newline */
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+		}
 	}
 	return nlines;
 }
 
-/* writelines: write output lines */
-void writelines(char *lineptr[], int nlines) {
-	int i;
-
-	for (i = 0; i < nlines; i++)
-		printf("%s\n", lineptr[i]);
+/* alloc: return pointer to n characters */
+char *alloc(int n) {
+	if (allocbuf + ALLOCSIZE - allocp >= n) {	/* if it fits */
+		allocp += n;
+		return allocp - n;			/* return old p */
+	} else
+		return 0;
 }
 
-/* getline: get a line */
-int getline (char *s, int lim) {
+/* getline_: get a line */
+int getline_ (char *s, int lim) {
 	int i, c;
 
 	i = 0;
@@ -124,3 +131,49 @@ int getline (char *s, int lim) {
 * in the same mannner as in our earlier examples, and writelines can be written
 * instead as
 */
+
+/* writelines: write output lines */
+void writelines(char *lineptr[], int nlines) {
+	while (nlines-- > 0)
+		printf("%s\n", *lineptr++);
+}
+
+/***
+ * Initially, *lineptr points to the first line; each element advances it to
+ * the next line pointer while nlines is counted down.
+ *
+ * With input and output under control, we can proceed to sorting.
+ */
+
+/* qsort: sort v[left]...v[right] into increasing order */
+void qsort(char *v[], int left, int right) {
+	int i, last;
+	void swap(char *v[], int i, int j);
+
+	if (left >= right)	/* do nothing if array contains */
+		return;		/* fewer than two elements */
+	swap(v, left, (left + right) / 2);
+	last = left;
+	for (i = left + 1; i <= right; i++)
+		if (strcmp(v[i], v[left]) < 0)
+			swap(v, ++last, i);
+	swap(v, left, last);
+	qsort(v, left, last - 1);
+	qsort(v, last + 1, right);
+}
+
+/***
+ * Similarly, the swap routine needs only trivial changes:
+ */
+
+/* swap: interchange v[i] and v[j] */
+void swap(char *v[], int i, int j) {
+	char *temp;
+
+	temp = v[i], v[i] = v[j], v[j] = temp;
+}
+
+/***
+ * Since any individual element of v (alias lineptr) is a character pointer,
+ * temp must be also, so one can be copied to the other.
+ */
