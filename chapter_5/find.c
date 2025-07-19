1cacd824a705f8597bb86d4c99f1492709ef961a
cankocak <can_kocak1@epam.com>
2025-07-19 08:13:55 +0300

    added -x -n optional flags and modified test.txt 🐒

--- chapter_5/find.c
+++ chapter_5/find.c
@@ -3,55 +3,55 @@
  * pattern-finding program so the pattern to be matched is specified by the
  * first argument on the command line.
  */
-#include <stdio.h>
-#include <string.h>
-#define MAXLINE 1000
-
-int getline_(char *line, int max) {
-  int i, c;
-
-  i = 0;
-  while (--max > 0 && (c = getchar()) != EOF && c != '\n') {
-    *line++ = c;
-    i++;
-  }
-
-  if (c == '\n') {
-    *line++ = c;
-    i++;
-  }
-
-  *line = '\0';
-  return i;
-}
-
-char *strindex_(char *s, char *t) {
-  char *ps, *pt;
-
-  for (; *s; s++) {
-    for (ps = s, pt = t; *pt && *ps == *pt; ps++, pt++)
-      ;
-    if (pt > t && *pt == '\0')
-      return s;
-  }
-  return NULL;
-}
+// #include <stdio.h>
+// #include <string.h>
+// #define MAXLINE 1000
+//
+// int getline_(char *line, int max) {
+//   int i, c;
+//
+//   i = 0;
+//   while (--max > 0 && (c = getchar()) != EOF && c != '\n') {
+//     *line++ = c;
+//     i++;
+//   }
+//
+//   if (c == '\n') {
+//     *line++ = c;
+//     i++;
+//   }
+//
+//   *line = '\0';
+//   return i;
+// }
+//
+// char *strindex_(char *s, char *t) {
+//   char *ps, *pt;
+//
+//   for (; *s; s++) {
+//     for (ps = s, pt = t; *pt && *ps == *pt; ps++, pt++)
+//       ;
+//     if (pt > t && *pt == '\0')
+//       return s;
+//   }
+//   return NULL;
+// }
 
 /* find: print lines that match pattern from 1st arg */
-int main(int argc, char *argv[]) {
-  char line[MAXLINE];
-  int found = 0;
-
-  if (argc != 2)
-    printf("Usage: find pattern\n");
-  else
-    while (getline_(line, MAXLINE) > 0)
-      if (strindex_(line, argv[1]) != NULL) {
-        printf("%s", line);
-        found++;
-      }
-  return found;
-}
+// int main(int argc, char *argv[]) {
+//   char line[MAXLINE];
+//   int found = 0;
+//
+//   if (argc != 2)
+//     printf("Usage: find pattern\n");
+//   else
+//     while (getline_(line, MAXLINE) > 0)
+//       if (strindex_(line, argv[1]) != NULL) {
+//         printf("%s", line);
+//         found++;
+//       }
+//   return found;
+// }
 
 /***
  * The standard library function strstr(s, t) returns a pointer to the first
@@ -71,4 +71,67 @@ int main(int argc, char *argv[]) {
  * 	find -nx pattern
  * Here is the program:
  */
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int getline_(char *s, int lim) {
+	int i, c;
+
+	i = 0;
+	while (--lim > 0 && (c = getchar()) != EOF && c != '\n') {
+		*s++ = c;
+		i++;
+	}
+
+	if (c == '\n') {
+		*s++ = c;
+		i++;
+	}
+
+	*s = '\0';
+	return i;
+}
+
+/* find: print line that match pattern from 1st arg */
+int main(int argc, char *argv[]) {
+	char line[MAXLINE];
+	long lineno = 0;
+	int c, except = 0, number = 0, found = 0;
+
+	while (--argc > 0 && (*++argv)[0] == '-')
+		// while ((c = *++argv[0]) != '\0')
+		while (c = *++argv[0])	/* check the next char */
+			switch (c) {
+			case 'x':
+				except = 1;
+				break;
+			case 'n':
+				number = 1;
+				break;
+			default:
+				printf("find: illegal option %c\n", c);
+				argc = 0;
+				found = -1;
+				break;
+			}
+	if (argc != 1)
+		printf("Usage: find -x -n pattern\n");
+	else
+		while (getline_(line, MAXLINE) > 0) {
+			lineno++;
+			if ((strstr(line, *argv) != NULL) != except) {
+				if (number)
+					printf("%ld:", lineno);
+				printf("%s", line);
+				found++;
+			}
+		}
+	return found;
+}
+
+/***
+ * argc is decremented and argv is incremented before each optional argument. At the end of the loop, if there are no errors, argc tells how many arguments remain unprocessed and argv points to the first of these. Thus argc should be 1 and *argv should point at the pattern. Notice that *++argv is a pointer to an argument string, so (*++argv)[0] is its first character. (An alternate valid form would be **++argv.) Because [] binds tighter than * and ++, the parantheses are necessary; without them the expression would be taken as *++(argv[0]). In fact, that is what we have used in the inner loop, the expression *++argv[0] increments the pointer argv[0]!
+ *
+ * It is rare than one uses pointer expressions more complicated than these; in such cases, breaking them into two or three steps will be more intuitive.
  */
