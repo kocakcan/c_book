48e14d557f3bff18f8c8df4c542e187d4c570e83
cankocak <can_kocak1@epam.com>
2025-07-21 13:52:03 +0300

    started changing array subscripts to pointer and an offset 🌚

--- chapter_5/5-10.c
+++ chapter_5/5-10.c
@@ -5,3 +5,12 @@
  * 	expr 2 3 4 + *
  * evaluates 2 * (3 + 4).
  */
+#include <stdio.h>
+
+/* expr: evaluate reverse Polish expression from the command line */
+int main(int argc, char *argv[]) {
+  if (argc == 1)
+    printf("expr: Reverse Polish expression evaluator 😎\n");
+
+  return 0;
+}
