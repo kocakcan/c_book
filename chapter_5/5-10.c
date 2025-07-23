a8e31f54806e8fa56295ae2fddc706973422f77b
cankocak <can_kocak1@epam.com>
2025-07-23 16:17:31 +0300

    can use existing Polish calculator with some modifications 🤔

--- chapter_5/5-10.c
+++ chapter_5/5-10.c
@@ -5,10 +5,16 @@
  * 	expr 2 3 4 + *
  * evaluates 2 * (3 + 4).
  */
+#include "../chapter_4/calc.h"
 #include <stdio.h>
+#include <stdlib.h>
+#define MAXOP 100
 
 /* expr: evaluate reverse Polish expression from the command line */
 int main(int argc, char *argv[]) {
+  int type;
+  double op2;
+  char s[MAXOP];
   if (argc == 1)
     printf("expr: Reverse Polish expression evaluator 😎\n");
 
