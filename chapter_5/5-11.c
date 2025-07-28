b6cf4da496267b234aea4d1509dc744ebea5c023
cankocak <can_kocak1@epam.com>
2025-07-28 12:04:18 +0300

    started working on it 🫡

--- chapter_5/5-11.c
+++ chapter_5/5-11.c
@@ -1,2 +1,39 @@
 /* Modify the program entab and detab to accept a list of tab stops as
  * arguments. Use the default tab settings if there are no arguments. */
+#include <stdio.h>
+#define TABSTOP 8
+
+int main(int argc, char *argv[]) {
+  int c, nspaces = 0, pos = 1;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      ++nspaces;
+      if (pos % TABSTOP == 0) {
+        // putchar('\t');
+        printf("[TAB]");
+        nspaces = 0;
+      }
+      ++pos;
+    } else {
+      while (nspaces > 0) {
+        // putchar(' ');
+        printf("[SPACE]");
+        nspaces--;
+      }
+
+      putchar(c);
+
+      if (c == '\n') {
+        pos = 1;
+        nspaces = 0;
+      } else if (c == '\t') {
+        pos += (TABSTOP - ((pos - 1) % TABSTOP));
+      } else
+        pos++;
+    }
+  }
+
+  while (nspaces--)
+    printf("[SPACE]");
+}
