942f99ffb5c7887a9d3d9011d0697367c8430a25
cankocak <can_kocak1@epam.com>
2025-08-01 16:18:59 +0300

    added a comment for the last while loop 😎

--- chapter_5/5-11.c
+++ chapter_5/5-11.c
@@ -5,10 +5,12 @@
 #define DEFAULT_TABSTOP 8
 #define MAX_TABS 100
 
+/* next_tab: returns next tabstop after (or at) current position */
 int next_tab(int pos, int *tabstops, int ntabs) {
   for (int i = 0; i < ntabs; i++)
     if (pos < tabstops[i])
       return tabstops[i];
+  // beyond specified tabs, use default intervals
   int last = ntabs > 0 ? tabstops[ntabs - 1] : DEFAULT_TABSTOP;
   int offset = (pos - last) % DEFAULT_TABSTOP;
   return pos + (DEFAULT_TABSTOP - offset);
@@ -55,6 +57,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  // on EOF
   while (nspaces > 0) {
     int next = next_tab(pos, tabstops, ntabs);
     if (pos + nspaces >= next) {
