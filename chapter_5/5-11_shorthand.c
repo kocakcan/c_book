a5dd02c04687eeebf8ce0c371532b3488a146493
cankocak <can_kocak1@epam.com>
2025-08-07 19:02:04 +0300

    added the pointer version 🫡

--- chapter_5/5-11_shorthand.c
+++ chapter_5/5-11_shorthand.c
@@ -8,27 +8,25 @@
 #define DEFAULT_TABSTOP 8
 #define MAX_TABS 100
 
+int tabstops[MAX_TABS];
+int *tp = tabstops;
+
 /* next_tab: returns next tabstop after (or at) current position */
-int next_tab(int pos, int *tabstops, int ntabs) {
-  for (int i = 0; i < ntabs; i++)
-    if (pos < tabstops[i])
-      return tabstops[i];
-  // beyond specified tabs, use default intervals
-  int last = ntabs > 0 ? tabstops[ntabs - 1] : DEFAULT_TABSTOP;
+int next_tab(int pos, int *tabstops) {
+  for (int *temp = tabstops; temp < tp; ++temp)
+    if (pos < *temp)
+      return *temp;
+  int last = (tp - tabstops > 0) ? *(tp - 1) : DEFAULT_TABSTOP;
   int offset = (pos - last) % DEFAULT_TABSTOP;
   return pos + (DEFAULT_TABSTOP - offset);
 }
 
 int main(int argc, char *argv[]) {
-  int tabstops[MAX_TABS];
-  int ntabs = 0;
-
   if (argc > 1) {
-    for (int i = 1; i < argc && ntabs < MAX_TABS; ++i)
-      tabstops[ntabs++] = atoi(argv[i]);
+    while (--argc > 0 && tp - tabstops < MAX_TABS)
+      *tp++ = atoi(*++argv);
   } else {
-    tabstops[0] = DEFAULT_TABSTOP;
-    ntabs = 1;
+    *tp++ = DEFAULT_TABSTOP;
   }
 
   int pos = 1, nspaces = 0, c;
@@ -38,7 +36,7 @@ int main(int argc, char *argv[]) {
       ++nspaces;
     else {
       while (nspaces > 0) {
-        int next = next_tab(pos, tabstops, ntabs);
+        int next = next_tab(pos, tabstops);
         if (pos + nspaces >= next) {
           // putchar('\t');
           printf("[TAB]");
@@ -62,7 +60,7 @@ int main(int argc, char *argv[]) {
 
   // on EOF
   while (nspaces > 0) {
-    int next = next_tab(pos, tabstops, ntabs);
+    int next = next_tab(pos, tabstops);
     if (pos + nspaces >= next) {
       // putchar('\t');
       printf("[TAB]");
