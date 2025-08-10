768d1e1dbdb72ef2a088c741802dac34463abcf2
cankocak <can_kocak1@epam.com>
2025-08-10 16:20:03 +0300

    added printf statement for the usage 🫡

--- chapter_5/5-11_shorthand.c
+++ chapter_5/5-11_shorthand.c
@@ -9,19 +9,24 @@
 int main(int argc, char *argv[]) {
   	int c, d, init_pos, every_col;
 
-    	if ((*++argv)[0] == '-')
-		if (c = atoi(*(argv) + 1))
-			init_pos = c;
+	if (argc == 1)
+		printf("Usage: -<starting_column> +<tabstop>\n");
+	else {
 
-	if ((*++argv)[0] == '+')
-		if (d = atoi(*(argv) + 1))
-			every_col = d;
+		if ((*++argv)[0] == '-')
+			if (c = atoi(*(argv) + 1))
+				init_pos = c;
 
-	// printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
-	// printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
+		if ((*++argv)[0] == '+')
+			if (d = atoi(*(argv) + 1))
+				every_col = d;
 
-  	printf("init_pos: %d | every_col: %d\n", init_pos, every_col);
-  	printf("\\0 is: %d\n", '\0');
+		// printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
+		// printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
+
+		printf("init_pos: %d | every_col: %d\n", init_pos, every_col);
+		printf("\\0 is: %d\n", '\0');
+	}
 
   return 0;
 }
