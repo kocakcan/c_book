2e17f563bf2d37f28faadb3e2ff819f659bd2722
cankocak <can_kocak1@epam.com>
2025-08-11 19:41:20 +0300

    it's over, i won 🧪

--- chapter_5/5-11_shorthand.c
+++ chapter_5/5-11_shorthand.c
@@ -5,34 +5,63 @@
  *  (for the user) default behaviour. */
 #include <stdio.h>
 #include <stdlib.h>
+#define DEFAULT_EVERY_COL 8
+#define DEFAULT_INIT_POS 1
+
+int next_tab(int pos, int init_pos, int every_col) {
+  int tab_width = (pos < init_pos) ? DEFAULT_EVERY_COL : every_col;
+  int distance = tab_width - (pos - 1) % tab_width;
+
+  return (distance > 0) ? distance : tab_width;
+}
 
 int main(int argc, char *argv[]) {
-  int c, init_pos, every_col;
+  int c, init_pos = DEFAULT_INIT_POS, every_col = DEFAULT_EVERY_COL;
+  int nspaces = 0, pos = 1;
 
-  if (argc == 1)
-    printf("Usage: -<starting_column> +<tabstop>\n");
-  else {
-
-    if ((*++argv)[0] == '-') {
-      if (c = atoi(*(argv) + 1))
-        init_pos = c;
+  while (--argc > 0) {
+    char *arg = *++argv;
+    if (*arg == '-' && *(arg + 1) != '\0') {
+      int val = atoi(arg + 1);
+      if (val > 0)
+        init_pos = val;
+    } else if (*arg == '+' && *(arg + 1) != '\0') {
+      int val = atoi(arg + 1);
+      if (val > 0)
+        every_col = val;
     } else {
-      exit(EXIT_FAILURE);
+      printf("error: unknown argument %c\n", *arg);
+      return 1;
     }
-
-    if ((*++argv)[0] == '+') {
-      if (c = atoi(*(argv) + 1))
-        every_col = c;
-    } else {
-      exit(EXIT_FAILURE);
-    }
-
-    // printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
-    // printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
-
-    printf("init_pos: %d | every_col: %d\n", init_pos, every_col);
-    printf("\\0 is: %d\n", '\0');
   }
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ')
+      ++nspaces;
+    else {
+      while (nspaces > 0) {
+        int next = next_tab(pos, init_pos, every_col);
+        if (nspaces >= next) {
+          printf("[TAB]");
+          nspaces -= next;
+          pos += next;
+        } else {
+          printf("[SPACE]");
+          --nspaces;
+          ++pos;
+        }
+      }
+
+      putchar(c);
+      if (c == '\n')
+        pos = 1;
+      else
+        ++pos;
+    }
+  }
+
+  while (nspaces--)
+    printf("[SPACE]");
 
   return 0;
 }
