696c8cdd503ac6a0add01ff6aeb7126609e53a5a
cankocak <can_kocak1@epam.com>
2025-08-11 18:08:51 +0300

    will finish this some day 😂

--- chapter_5/5-11_shorthand.c
+++ chapter_5/5-11_shorthand.c
@@ -7,26 +7,32 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  	int c, d, init_pos, every_col;
+  int c, init_pos, every_col;
 
-	if (argc == 1)
-		printf("Usage: -<starting_column> +<tabstop>\n");
-	else {
+  if (argc == 1)
+    printf("Usage: -<starting_column> +<tabstop>\n");
+  else {
 
-		if ((*++argv)[0] == '-')
-			if (c = atoi(*(argv) + 1))
-				init_pos = c;
+    if ((*++argv)[0] == '-') {
+      if (c = atoi(*(argv) + 1))
+        init_pos = c;
+    } else {
+      exit(EXIT_FAILURE);
+    }
 
-		if ((*++argv)[0] == '+')
-			if (d = atoi(*(argv) + 1))
-				every_col = d;
+    if ((*++argv)[0] == '+') {
+      if (c = atoi(*(argv) + 1))
+        every_col = c;
+    } else {
+      exit(EXIT_FAILURE);
+    }
 
-		// printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
-		// printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
+    // printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
+    // printf("1st char of 2nd arg string: %c\n", (*++argv)[0]);
 
-		printf("init_pos: %d | every_col: %d\n", init_pos, every_col);
-		printf("\\0 is: %d\n", '\0');
-	}
+    printf("init_pos: %d | every_col: %d\n", init_pos, every_col);
+    printf("\\0 is: %d\n", '\0');
+  }
 
   return 0;
 }
