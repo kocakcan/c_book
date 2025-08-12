66619ef4cac27b072f52b909b2df2ef48c89370e
cankocak <can_kocak1@epam.com>
2025-08-12 19:51:55 +0300

    not sure about forcing 2 args or no args 🤔

--- chapter_5/detab_shorthand.c
+++ chapter_5/detab_shorthand.c
@@ -3,3 +3,31 @@
  *   entab -m +n
  *  to mean tab stops every n columns, starting at column m. Choose convenient
  *  (for the user) default behaviour. */
+#include <stdio.h>
+#include <stdlib.h>
+#define DEFAULT_EVERY_COL 8
+#define DEFAULT_INIT_POS 1
+
+int main(int argc, char *argv[]) {
+  int c, init_pos = DEFAULT_INIT_POS, every_col = DEFAULT_EVERY_COL, pos = 1;
+
+  for (size_t i = 0; i < 2; ++i) {
+    char *arg = *++argv;
+    if (*arg == '-' && *(arg + 1) != '\0') {
+      int val = atoi(arg + 1);
+      if (val > 0)
+        init_pos = val;
+    } else if (*arg == '+' && *(arg + 1) != '\0') {
+      int val = atoi(arg + 1);
+      if (val > 0)
+        every_col = val;
+    } else {
+      printf("error: unknown argument %c\n", *arg);
+      return 1;
+    }
+  }
+
+  printf("init_pos: %d | every_col: %d\n", init_pos, every_col);
+
+  return 0;
+}
