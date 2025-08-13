099250ad3c98fcc4630f36bca96d15fdd81fe1d0
cankocak <can_kocak1@epam.com>
2025-08-13 16:44:48 +0300

    completed the implementation 🤔

--- chapter_5/detab_shorthand.c
+++ chapter_5/detab_shorthand.c
@@ -8,6 +8,13 @@
 #define DEFAULT_EVERY_COL 8
 #define DEFAULT_INIT_POS 1
 
+int next_tab(int pos, int every_col, int init_pos) {
+  int tab_width = (pos < init_pos) ? DEFAULT_EVERY_COL : every_col;
+  int distance = tab_width - (pos - 1) % tab_width;
+
+  return (distance > 0) ? distance : tab_width;
+}
+
 int main(int argc, char *argv[]) {
   int c, init_pos = DEFAULT_INIT_POS, every_col = DEFAULT_EVERY_COL, pos = 1;
 
@@ -27,7 +34,22 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  printf("init_pos: %d | every_col: %d\n", init_pos, every_col);
+  // printf("init_pos: %d | every_col: %d\n", init_pos, every_col);
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\t') {
+      int next = next_tab(pos, every_col, init_pos);
+      for (; pos < next; ++pos)
+        printf("[SPACE]");
+    } else {
+      putchar(c);
+
+      if (c == '\n')
+        pos = 1;
+      else
+        ++pos;
+    }
+  }
 
   return 0;
 }
