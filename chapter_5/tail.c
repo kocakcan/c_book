a670d63a0945e8fdb5d376e616b20210005055ee
cankocak <can_kocak1@epam.com>
2025-08-14 18:13:44 +0300

    added getline() 😎

--- chapter_5/tail.c
+++ chapter_5/tail.c
@@ -8,3 +8,28 @@
  * best use of available storage; lines should be stored as in the sorting
  * program, not in a two-dimensional array of fixed size.
  */
+#include <stdio.h>
+
+int getline_(char *s, int lim) {
+  int c, i = 0;
+
+  while (--lim > 0 && (c = getchar()) != EOF && c != '\n') {
+    *s++ = c;
+    i++;
+  }
+
+  if (c == '\n') {
+    *s++ = c;
+    i++;
+  }
+
+  *s = '\0';
+
+  return i;
+}
+
+int main(int argc, char *argv[]) {
+  printf("2nd arg string: %s\n", *++argv);
+
+  return 0;
+}
