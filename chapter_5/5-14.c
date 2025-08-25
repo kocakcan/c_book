9278437cefc15bf75cc8e9f01d25353b63cedf01
cankocak <can_kocak1@epam.com>
2025-08-25 09:58:17 +0300

    formatted the files 🫡

--- chapter_5/5-14.c
+++ chapter_5/5-14.c
@@ -1,9 +1,9 @@
 /* Modify the sort program to handle a -r flag, which indicates sorting in
  * reverse (decreasing) order. Be sure that -r works with -n.
  */
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #define ALLOCSIZE 10000
 #define MAXLINES 5000
 #define MAXLEN 1000
@@ -24,51 +24,51 @@ void qsort_(void *lineptr[], int left, int right, int (*comp)(void *, void *));
 int numcmp(char *, char *);
 int strcmp_(char *, char *);
 
-int _strcmp_(void *a, void *b) {
-	return strcmp_((char *)a, (char *)b);
-}
+int _strcmp_(void *a, void *b) { return strcmp_((char *)a, (char *)b); }
 
-int rstrcmp(void *a, void *b) {
-	return -strcmp_((char *)a, (char *)b);
-}
+int rstrcmp(void *a, void *b) { return -strcmp_((char *)a, (char *)b); }
 
 int is_number(const char *s) {
-	if (*s == '-' || *s == '+') s++;
-	int has_digit = 0, has_dot = 0;
-	while (*s) {
-		if (isdigit((unsigned char)*s))
-			has_digit = 1;
-		else if (*s == '.' && !has_dot)
-			has_dot = 1;
-		else
-			return 0;
-		s++;
-	}
+  if (*s == '-' || *s == '+')
+    s++;
+  int has_digit = 0, has_dot = 0;
+  while (*s) {
+    if (isdigit((unsigned char)*s))
+      has_digit = 1;
+    else if (*s == '.' && !has_dot)
+      has_dot = 1;
+    else
+      return 0;
+    s++;
+  }
 
-	return has_digit;
+  return has_digit;
 }
 
 int numcmp_with_fallback(char *s, char *t) {
-	if (is_number(s) && is_number(t)) {
-		double v1 = atof(s);
-		double v2 = atof(t);
-		if (v1 < v2) return -1;
-		else if (v1 > v2) return 1;
-		else return 0;
-	}
-	return strcmp_(s, t);
+  if (is_number(s) && is_number(t)) {
+    double v1 = atof(s);
+    double v2 = atof(t);
+    if (v1 < v2)
+      return -1;
+    else if (v1 > v2)
+      return 1;
+    else
+      return 0;
+  }
+  return strcmp_(s, t);
 }
 
 int rnumcmp_with_fallback(char *s, char *t) {
-	return -numcmp_with_fallback(s, t);
+  return -numcmp_with_fallback(s, t);
 }
 
 int cmp_numeric(void *a, void *b) {
-	return numcmp_with_fallback((char *)a, (char *)b);
+  return numcmp_with_fallback((char *)a, (char *)b);
 }
 
 int cmp_numeric_r(void *a, void *b) {
-	return rnumcmp_with_fallback((char *)a, (char *)b);
+  return rnumcmp_with_fallback((char *)a, (char *)b);
 }
 
 int main(int argc, char *argv[]) {
@@ -92,18 +92,18 @@ int main(int argc, char *argv[]) {
   }
 
   if (numeric)
-	cmp = reverse ? cmp_numeric_r : cmp_numeric;
+    cmp = reverse ? cmp_numeric_r : cmp_numeric;
   else
-	cmp = reverse ? rstrcmp : _strcmp_;
+    cmp = reverse ? rstrcmp : _strcmp_;
 
-	if ((nlines = readlines(lineptr, MAXLINES)) > 0) {
-	qsort_((void **) lineptr, 0, nlines - 1, cmp);
-		writelines(lineptr, nlines);
-		return 0;
-	} else {
-		printf("error: input too big to sort\n");
-		return 1;
-	}
+  if ((nlines = readlines(lineptr, MAXLINES)) > 0) {
+    qsort_((void **)lineptr, 0, nlines - 1, cmp);
+    writelines(lineptr, nlines);
+    return 0;
+  } else {
+    printf("error: input too big to sort\n");
+    return 1;
+  }
 
   return 0;
 }
@@ -187,19 +187,19 @@ int numcmp(char *s, char *t) {
     return 0;
 }
 
-void qsort_(void *v[], int left, int right, int (*comp) (void *, void *)) {
-	int i, last;
-	
-	if (left >= right)
-		return;
+void qsort_(void *v[], int left, int right, int (*comp)(void *, void *)) {
+  int i, last;
 
-	swap(v, left, (left + right) / 2);
-	last = left;
+  if (left >= right)
+    return;
 
-	for (i = left + 1; i <= right; i++)
-		if ((*comp)(v[i], v[left]) < 0)
-			swap(v, ++last, i);
-	swap(v, left, last);
-	qsort_(v, left, last - 1, comp);
-	qsort_(v, last + 1, right, comp);
+  swap(v, left, (left + right) / 2);
+  last = left;
+
+  for (i = left + 1; i <= right; i++)
+    if ((*comp)(v[i], v[left]) < 0)
+      swap(v, ++last, i);
+  swap(v, left, last);
+  qsort_(v, left, last - 1, comp);
+  qsort_(v, last + 1, right, comp);
 }
