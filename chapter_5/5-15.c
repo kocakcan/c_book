8833bd915608acdb9aebda03bbb2ba4130b1f955
cankocak <can_kocak1@epam.com>
2025-08-26 12:02:21 +0300

    added the function signatures

--- chapter_5/5-15.c
+++ chapter_5/5-15.c
@@ -1,3 +1,27 @@
 /* Add the option -f to fold upper and lower case together, so that case
  * distinctions are not made during sorting; For example, a and A compare equal.
  */
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 1000
+#define MAXLEN 5000
+#define ALLOCSIZE 10000
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+static char *lineptr[MAXLEN];
+char *alloc(int);
+
+int getline_(char *, int);
+int readlines(char **, int);
+void writelines(char **, int);
+int strcmp_(const char *, const char *);
+int numcmp(const char *, const char *);
+void qsort_(void **, int, int, int (*)(const void *, const void *));
+void swap(void **, int, int);
+
+int main(void) {
+  printf("aye, siwmae\n");
+  return 0;
+}
