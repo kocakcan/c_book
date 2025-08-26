90127450f6d95d4387f2443dcc52cba2e2e6562e
cankocak <can_kocak1@epam.com>
2025-08-26 18:14:57 +0300

    added all the functions for now 🙆‍♂️

--- chapter_5/5-15.c
+++ chapter_5/5-15.c
@@ -4,24 +4,111 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define MAXSIZE 1000
+#define MAXLINES 5000
 #define MAXLEN 5000
 #define ALLOCSIZE 10000
 
 static char allocbuf[ALLOCSIZE];
 static char *allocp = allocbuf;
-static char *lineptr[MAXLEN];
+static char *lineptr[MAXLINES];
 char *alloc(int);
 
 int getline_(char *, int);
+void strcpy_(char *, const char *);
 int readlines(char **, int);
 void writelines(char **, int);
 int strcmp_(const char *, const char *);
 int numcmp(const char *, const char *);
-void qsort_(void **, int, int, int (*)(const void *, const void *));
 void swap(void **, int, int);
+void qsort_(void **, int, int, int (*)(const void *, const void *));
 
 int main(void) {
   printf("aye, siwmae\n");
   return 0;
+}
+
+char *alloc(int n) {
+  if (allocbuf + ALLOCSIZE - allocp >= n) {
+    allocp += n;
+    return allocp - n;
+  }
+  return 0;
+}
+
+int getline_(char *s, int lim) {
+  int c, i = 0;
+
+  while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+    *s++ = c, i++;
+
+  if (c == '\n')
+    *s++ = c, i++;
+  *s = '\0';
+  return i;
+}
+
+void strcpy_(char *s, const char *t) {
+  while (*s++ = *t++)
+    ;
+}
+
+int readlines(char **lineptr, int maxlines) {
+  int len, nlines = 0;
+  char line[MAXLEN], *p;
+
+  while ((len = getline_(line, MAXLEN)) > 0) {
+    if (nlines >= maxlines || (p = alloc(len)) == NULL)
+      return -1;
+    line[len - 1] = '\0';
+    strcpy_(p, line);
+    lineptr[nlines++] = p;
+  }
+
+  return nlines;
+}
+
+void writelines(char **lineptr, int nlines) {
+  while (nlines--)
+    printf("%s\n", *lineptr++);
+}
+
+int strcmp_(const char *s, const char *t) {
+  while (*s && *s == *t)
+    s++, t++;
+  return (unsigned char)*s - (unsigned char)*t;
+}
+
+int numcmp(const char *s, const char *t) {
+  double v1 = atof(s), v2 = atof(t);
+
+  if (v1 < v2)
+    return -1;
+  else if (v1 > v2)
+    return 1;
+  else
+    return 0;
+}
+
+void swap(void **v, int left, int right) {
+  void *temp;
+
+  temp = *(v + left), *(v + left) = *(v + right), *(v + right) = temp;
+}
+
+void qsort_(void **v, int left, int right,
+            int (*comp)(const void *, const void *)) {
+  int i, last;
+
+  if (left >= right)
+    return;
+
+  swap(v, left, (left + right) / 2);
+  last = left;
+
+  for (i = left + 1; i <= right; i++)
+    if ((*comp)(v[i], v[left]) < 0)
+      swap(v, ++last, i);
+  swap(v, left, last);
+  qsort_(v, left, last - 1, comp);
+  qsort_(v, last + 1, right, comp);
 }
