f0dbbb8d76b806d98321750c75a77e4ac1b21b26
cankocak <can_kocak1@epam.com>
2025-09-05 07:43:14 +0300

    added the question for 5-17 🐒

---
+++ chapter_5/5-17.c
@@ -0,0 +1,2 @@
+/* Add a field-searching capability, so sorting may be done on fields within lines, each field sorted according to an independent set of options. (The index for this book was sorted with -df for the index category and -n for the page numbers.)
+ */
