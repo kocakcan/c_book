8017c87a47a900eb2a25b6e28751853544978427
cankocak <can_kocak1@epam.com>
2025-10-05 14:29:48 +0300

    wait this is so cool 😎

--- chapter_5/complicated_declarations.c
+++ chapter_5/complicated_declarations.c
@@ -29,4 +29,137 @@
  * 	x: array[3] of pointer to function returning
  * 	pointer to array[5] of char
  * dcl is based on the grammar that specifies a declarator.
+ *
+ * dcl:		optional *'s direct-dcl
+ * direct-dl name
+ * 			(dcl)
+ * 			direct-dcl()
+ * 			direct-dlc[optional size]
+ *
+ * In words, a dcl is a direct-dcl, perhaps preceeded by *'s. A direct-dcl is a name, or a paranthesized dcl, or a direct-dcl followed by parantheses, or a direct-dcl followed by brackets with an optional size.
+ *
+ * This grammar can be used to parse functions. For example, consider this declarator:
+ *
+ * 	(*pfa[])()
+ * pfa will be identified as a name and thus as a direct-dcl. Then pfa[] is also a direct-dcl. Then *pfa[] is recognized as a dcl, so (*pfa[])() is a direct-dcl and thus a del.
+ *
+ * The heart of the dcl program is a pair of functions, dcl and dirdcl, that parse a declaration according to this grammar. Because the grammar is recursively defined, the functions call each other recursively as the recognize pieces of a declaration; the program is called a recursive-descent parser.
  */
+
+/***
+ * Since the program are intended to be illustrative, not bullet-proof, there are significant restrictions on dcl. It can only handle a simple data type line char or int. It does not handle argument types in functions, or qualifiers like const. Spurious blanks confuse it. It doesn't do much error recovery, so invalid declarations will also confuse it. These improvements are left as exercises.
+ *
+ * Here are the global variables and the main routine:
+ */
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXTOKEN	100
+#define BUFSIZE		100
+enum { NAME, PARENS, BRACKETS };
+
+static char buf[BUFSIZE];
+static char *bufp = buf;
+
+void dcl(void);
+void dirdcl(void);
+
+int gettoken(void);
+int tokentype;			/* type of last token */
+char token[MAXTOKEN];		/* last token string */
+char name[MAXTOKEN];		/* identifier name */
+char datatype[MAXTOKEN];	/* data type = char, int, etc. */
+char out[1000];
+
+/* convert declaration to words */
+int main(void) {
+	while (gettoken() != EOF) {	/* 1st token on line */
+		strcpy(datatype, token);	/* is the datatype */
+		out[0] = '\0';
+		dcl();			/* parse rest of line */
+		if (tokentype != '\n')
+			printf("syntax error\n");
+		printf("%s: %s %s\n", name, out, datatype);
+	}
+
+	return 0;
+}
+
+/* dirdcl: parse a direct declarator */
+void dirdcl(void) {
+	int type;
+
+	if (tokentype == '(') {
+		dcl();
+		if (tokentype != ')')
+			printf("error: missing )\n");
+	} else if (tokentype == NAME)	/* variable name */ {
+		strcpy(name, token);
+	} else
+		printf("error: expected name or (dcl)\n");
+	while ((type = gettoken()) == PARENS || type == BRACKETS)
+		if (type == PARENS)
+			strcat(out, " function returning");
+		else {
+			strcat(out, " array");
+			strcat(out, token);
+			strcat(out, " of");
+		}
+}
+
+/* dcl: parse a declarator */
+void dcl(void) {
+	int ns;
+
+	for (ns = 0; gettoken() == '*';)	/* count *'s */
+		ns++;
+	dirdcl();
+	while (ns-- > 0)
+		strcat(out, " pointer to");
+}
+
+/***
+ * The function gettoken skips blanks and tabs, then finds the next token in the input; a "token" is a name, a pair of parantheses, a pair of brackets perhaps including a number, or any other single character.
+ */
+
+int gettoken(void) {	/* return next token */
+	int c, getch(void);
+	void ungetch(int);
+	char *p = token;
+
+	while ((c = getch()) == ' ' || c == '\t')
+		;
+	if (c == '(') {
+		if ((c = getch()) == ')') {
+			strcpy(token, "()");
+			return tokentype = PARENS;
+		} else {
+			ungetch(c);
+			return tokentype = '(';
+		}
+	} else if (c == '[') {
+		for (*p++ = c; (*p++ = getch()) != ']';)
+			;
+		*p = '\0';
+		return tokentype = BRACKETS;
+	} else if (isalpha(c)) {
+		for (*p++ = c; isalnum(c = getch());)
+			*p++ = c;
+		*p = '\0';
+		ungetch(c);
+		return tokentype = NAME;
+	} else
+		return tokentype = c;
+}
+
+int getch(void) {
+	return (bufp - buf > 0) ? *--bufp : getchar();
+}
+
+void ungetch(int c) {
+	if (bufp - buf >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		*bufp++ = c;
+}
