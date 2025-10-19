906733b43250d1175751e9e73d08e3988d950737
cankocak <can_kocak1@epam.com>
2025-10-19 08:15:17 +0300

    completed 6.2 Structures and Functions 🫡

--- chapter_6/structures_and_functions.c
+++ chapter_6/structures_and_functions.c
@@ -78,10 +78,49 @@
  *    temp.pt2.y = max(r.pt1.y, r.pt2.y);
  *    return temp;
  *  }
+ * If a large structure is to be passed to a function, it is generally more efficient to pass a pointer than to copy the whole structure. Structure pointers are just like pointers to ordinary variables. The declaration
+ * 	
+ * 	struct point *pp;
+ * says that pp is a pointer to a structure of type struct point. If pp points to a point structure, *pp is the structure, and (*pp).x and (*pp).y are the members. To use pp, we might write, for example,
+ *
+ * 	struct point origin, *pp;
+ * 	pp = &origin;
+ * 	printf("origin is (%d,%d)\n", (*pp).x, (*pp).y);
+ * The parantheses are necessary in (*pp).x because the precedence of the structure member operator . is higher than *. The expression *pp.x means *(pp.x), which is illegal here because x is not a pointer.
+ *
+ * Pointers to structures are so frequently used that an alternative notation is provided as a shorthand. If p is a pointer to a structure, then
+ *
+ * 	p->member-of-structure
+ * refers to the particular member. So we could write instead
+ *
+ * 	printf("origin is (%d,%d)\n", pp->x, pp->y);
+ * Both . and -> associate from left to right, so if we have
+ *
+ * 	struct rect r, *rp = &r;
+ * then these four expression are equivalent:
+ *
+ * 	r.pt1.x
+ * 	rp->pt1.x
+ * 	(r.pt1).x
+ * 	(rp->pt1).x
+ * The structure operators . and ->, together with () for function calls and [] for subscripts are at the top of the precedence hierarchy and thus bind very tightly. For example, given the declaration
+ *
+ * 	struct {
+ * 		int len;
+ * 		char *str;
+ * 	} *p;
+ * then
+ *
+ * 	++p->len
+ * increments len, not p, because the implied parenthesization is ++(p->len). Parentheses can be used to alter binding: (++p)->len increments p before accessing len, and (p++)->len increments p afterward. (The last set of parentheses are unnecessary)
+ *
+ * In the same way, *p->str fetches whatever str points to; *p->str++ increments str after accessing whatever it points to (just like *s++); (*p->str)++ increments whatever str points to; and *p++->str increments p after accessing whatever str points to.
  */
 #include <stdio.h>
 #define XMAX 1920
 #define YMAX 1200
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 struct point {
   int x;
@@ -115,6 +154,17 @@ int ptinrect(struct point p, struct rect r) {
 	    && p.y >= r.pt1.y && p.y < r.pt2.y;
 }
 
+/* canonrect: canonicalize coordinates of rectangle */
+struct rect canonrect(struct rect r) {
+	struct rect temp;
+
+	temp.pt1.x = min(r.pt1.x, r.pt2.x);
+	temp.pt1.y = min(r.pt1.y, r.pt2.y);
+	temp.pt2.x = max(r.pt1.x, r.pt2.x);
+	temp.pt2.y = max(r.pt1.y, r.pt2.y);
+	return temp;
+}
+
 int main(void) {
   struct rect screen;
   struct point middle;
@@ -129,9 +179,12 @@ int main(void) {
   struct point pt3 = addpoints(screen.pt1, screen.pt2);
   struct point pt4 = makepoint(197, 42);
   struct point pt5 = makepoint(15, 1921);
+  struct rect canrect = canonrect(screen);
   printf("screen.pt1 + screen.pt2 = (%d, %d)\n", pt3.x, pt3.y);
   printf("pt4(%d, %d) %s in screen\n", pt4.x, pt4.y, (ptinrect(pt4, screen) == 1) ? "is" : "is not");
   printf("pt5(%d, %d) %s in screen\n", pt5.x, pt5.y, (ptinrect(pt5, screen) == 1) ? "is" : "is not");
+  printf("canrect((%d, %d), (%d, %d))\n", canrect.pt1.x, 
+	canrect.pt1.y, canrect.pt2.x, canrect.pt2.y);
 
   return 0;
 }
