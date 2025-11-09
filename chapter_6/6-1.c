c695edc1dc9806e1d791622ab30cfc01f462e9fd
cankocak <can_kocak1@epam.com>
2025-11-09 19:57:40 +0300

    completed 6-1 🐒

--- chapter_6/6-1.c
+++ chapter_6/6-1.c
@@ -3,15 +3,60 @@
  */
 #include <ctype.h>
 #include <stdio.h>
+
 #define MAXWORD 100
+#define NKEYS (sizeof keytab / sizeof(keytab[0]))
+
+struct key {
+  char *word;
+  int count;
+} keytab[] = {"auto",  0, "break",    0, "case",    0, "char",     0,
+              "const", 0, "continue", 0, "default", 0, "unsigned", 0,
+              "void",  0, "volatile", 0, "while",   0};
+
+int getch(void);
+void ungetch(int);
+int strcmp_(const char *, const char *);
+int comment(void);
+int getword(char *, int);
+int binsearch(char *, struct key *, int);
 
 static char buf[MAXWORD];
 static char *bufp = buf;
 
-int getch(void);
-void ungetch(int);
-int comment(void);
-int getword(char *, int);
+/* count C keywords */
+int main() {
+  int n;
+  char word[MAXWORD];
+
+  while (getword(word, MAXWORD) != EOF)
+    if (isalpha(word[0]))
+      if ((n = binsearch(word, keytab, NKEYS)) >= 0)
+        keytab[n].count++;
+  for (n = 0; n < NKEYS; n++)
+    if (keytab[n].count > 0)
+      printf("%4d %s\n", keytab[n].count, keytab[n].word);
+}
+
+/* binsearch: find word int tab[0]...tab[n-1] */
+int binsearch(char *word, struct key tab[], int n) {
+  int cond;
+  int low, high, mid;
+
+  low = 0;
+  high = n - 1;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if ((cond = strcmp_(word, tab[mid].word)) < 0)
+      high = mid - 1;
+    else if (cond > 0)
+      low = mid + 1;
+    else
+      return mid;
+  }
+
+  return -1;
+}
 
 /* getword: get next word or character from input */
 int getword(char *word, int lim) {
@@ -69,4 +114,11 @@ void ungetch(int c) {
     printf("ungetch: too many characters\n");
   else
     *bufp++ = c;
+}
+
+int strcmp_(const char *s, const char *t) {
+  for (; *s == *t; s++, t++)
+    if (*s == '\0')
+      return 0;
+  return *s - *t;
 }
