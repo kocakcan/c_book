d3aa4a964291db4af176b92376d438f4d286ed9b
cankocak <can_kocak1@epam.com>
2025-11-23 19:25:40 +0300

    added the first paragraph 🫜

--- chapter_6/pointers_to_structures.c
+++ chapter_6/pointers_to_structures.c
@@ -1,3 +1,7 @@
 /***
  * 6.4 Pointers to Structures
+ *
+ * To illustrate some of the considerations involved with pointers to arrays of
+ * structures, let us write the keyword-counting program again, this time using
+ * pointers instead of array indices.
  */
