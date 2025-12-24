f934ea81d99f9a7ee1ccbbdb4fe8e524c343b739
cankocak <can_kocak1@epam.com>
2025-12-24 21:53:47 +0300

    what magic is this 🧙🏻‍♂️

---
+++ chapter_6/wow.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+unsigned int *salmon =
+    U"󠁓󠁥󠁡󠁳󠁯󠁮󠀠󠁳󠁡󠁬󠁭󠁯󠁮󠀠󠁦󠁩󠁬󠁬"
+    U"󠁥󠁴󠁳󠀠󠁷󠁩󠁴󠁨󠀠󠁯󠁬󠁩󠁶󠁥󠀠󠁯󠁩󠁬"
+    U"󠀬󠀠󠁬󠁥󠁭󠁯󠁮󠀠󠁪󠁵󠁩󠁣󠁥󠀬󠀠󠁧󠁡󠁲"
+    U"󠁬󠁩󠁣󠀬󠀠󠁳󠁡󠁬󠁴󠀬󠀠󠁡󠁮󠁤󠀠󠁰󠁥󠁰"
+    U"󠁰󠁥󠁲󠀬󠀠󠁴󠁨󠁥󠁮󠀠󠁧󠁲󠁩󠁬󠁬󠀠󠁯󠁶"
+    U"󠁥󠁲󠀠󠁭󠁥󠁤󠁩󠁵󠁭󠀠󠁨󠁥󠁡󠁴󠀠󠁦󠁯󠁲"
+    U"󠀠󠀴󠀭󠀶󠀠󠁭󠁩󠁮󠁵󠁴󠁥󠁳󠀠󠁰󠁥󠁲󠀠󠁳"
+    U"󠁩󠁤󠁥󠀠󠁵󠁮󠁴󠁩󠁬󠀠󠁦󠁬󠁡󠁫󠁹󠀠󠁡󠁮"
+    U"󠁤󠀠󠁣󠁯󠁯󠁫󠁥󠁤󠀠󠁴󠁨󠁲󠁯󠁵󠁧󠁨󠀮   is"
+    U" very yummy";
+
+#define grill󠁁                                                              \
+  ;                                                                            \
+  while (putchar(*salmon++))
+#undef grill
+
+#define 󠁁 grill󠁁
+#undef grill
+
+int main() {
+  󠁁 󠁁 for (int i = 1; i <= 10; i++) {
+    printf("%d * %d = %d\n", i, i, i * i);
+  }
+}
