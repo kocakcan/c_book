7b95d02cdf436648f1723d1de4cd1252d47c448e
cankocak <can_kocak1@epam.com>
2025-12-28 20:05:16 +0300

    added the Python code for filter() implementation 🎅🏻

---
+++ chapter_6/filter.c
@@ -0,0 +1,7 @@
+// def main():
+//     numbers: list[int] = [1, 2, 3, 4, 5]
+//     print(list(filter(lambda x: x % 2 != 0, numbers)))
+//
+// if __name__ == "__main__":
+//     main()
+// TODO: Implement filter in C
