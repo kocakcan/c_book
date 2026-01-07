a41d068965ffe2ef481e52b5eb93449282958c4b
cankocak <can_kocak1@epam.com>
2026-01-07 19:29:14 +0300

    added the question for 6-6 exercise ☃️

---
+++ chapter_6/6-6.c
@@ -0,0 +1,5 @@
+/***
+ * Implement a simple version of the #define processor (i.e., no arguments)
+ * suitable for use with C programs, based on the routines of this section. You
+ * may also find getch and ungetch helpful.
+ */
