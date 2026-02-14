b1ad497e1a098984017346d5455669b96ab8db31
cankocak <can_kocak1@epam.com>
2026-02-14 19:50:42 +0300

    completed Bit-field

--- chapter_6/bit_fields.c
+++ chapter_6/bit_fields.c
@@ -26,4 +26,77 @@
  * of "bit-fiddling" with the shifting, masking, and complementing operators.
  *
  * Certain idioms appear frequently:
+ *
+ *  flags |= EXTERNAL | STATIC;
+ * turns on the EXTERNAL and STATIC bits in flags, while
+ *
+ *  flags &= ~(EXTERNAL | STATIC);
+ * turns them off, and
+ *
+ *  if ((flags & (EXTERNAL | STATIC)) == 0) ...
+ * is true if both bits are off.
+ *
+ * Although these idioms are readily mastered, as an alternative C offers the
+ * capability of defining and accessing fields within a word directly rather
+ * than bitwise logical operators. A bit-field, or field for short, is a set of
+ * adjacent bits withing a single implementation-defined storage unit that we
+ * will call a "word". For example, the symbol table #defines above could be
+ * replaced by the definition of three fields:
+ *
+ *  struct {
+ *    unsigned int is_keyword : 1;
+ *    unsigned int is_extern  : 1;
+ *    unsigned int is_static  : 1;
+ *  } flags;
+ * This defines a variable table called flags that contain three 1-bit fields.
+ * The number following the colon represents the field width in bits. The fields
+ * are declared unsigned int to ensure that they are unsigned quantities.
+ *
+ * Individual fields are referenced in the same way as other structure members:
+ * flags.is_keyword, flags.is_extern, etc. Fields behave like small integers,
+ * and may participate in arithmetic expressions just like other integers. Thus
+ * the previous examples may be written more naturally as
+ *
+ *  flags.is_extern = flags.is_static = 1;
+ * to turn the bits on;
+ *
+ *  flags.is_extern = flags.is_static = 0;
+ * to turn them off; and
+ *
+ *  if (flags.is_extern == 0 && flags.is_static == 0)
+ *    ...
+ * to test them.
+ *
+ * Almost everything about fields is implementation-dependent. Whether a field
+ * may overlap a word boundary is implementation-defined. Fields need not be
+ * names; unnamed fields (a colon and width only) are used for padding. The
+ * special width 0 may be used to force alignment at the next word boundary.
+ *
+ * Fields are assigned left to right on some machines and right to left on
+ * others. This means that although fields are useful for maintaining
+ * internally-defined data structures, the question of which end comes first has
+ * to be carefully considered when picking apart externally-defined data;
+ * programs that depend on such thing are not portable. Fields may be declared
+ * only as ints; for portability, specify signed or unsigned explicitly. They
+ * are not arrays and they do not have addresses, so the & operator cannot be
+ * applied on them.
  */
+#include <stdio.h>
+
+struct {
+  unsigned int is_keyword : 1;
+  unsigned int is_extern : 1;
+  unsigned int is_static : 1;
+} flags;
+
+int main(void) {
+  // turn the bits on
+  flags.is_extern = flags.is_static = 1;
+
+  // turn the bits off
+  flags.is_extern = flags.is_static = 0;
+
+  if (flags.is_extern == 0 && flags.is_static == 0)
+    printf("It's a keyword matey!\n");
+  return 0;
+}
