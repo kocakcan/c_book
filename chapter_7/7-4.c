bee9010bf474f4c937cc8741aa1af712651e63fb
cankocak <can_kocak1@epam.com>
2026-02-21 08:26:10 +0300

    started implementing minscanf

--- chapter_7/7-4.c
+++ chapter_7/7-4.c
@@ -1,2 +1,41 @@
 /* Write a private version of scanf analogous to minprintf from the previous
  * section. */
+#include <stdarg.h>
+#include <stdio.h>
+
+void minscanf(char *, ...);
+
+int main(void) { return 0; }
+
+void minscanf(char *fmt, ...) {
+  va_list ap;
+  char *p, *sval;
+  double dval;
+  int ival;
+
+  va_start(ap, fmt);
+  for (p = fmt; *p; p++) {
+    if (*p != '%') {
+      getchar();
+      continue;
+    }
+    switch (*++p) {
+    case 'd':
+      ival = va_arg(ap, int);
+      scanf("%d", &ival);
+      break;
+    case 'f':
+      dval = va_arg(ap, double);
+      scanf("%lf", &dval);
+      break;
+    case 's':
+      for (sval = va_arg(ap, char *); *sval; sval++)
+        getchar();
+      break;
+    default:
+      getchar();
+      break;
+    }
+  }
+  va_end(ap);
+}
