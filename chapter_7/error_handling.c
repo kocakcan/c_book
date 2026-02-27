1d38a9245f6ce0206dfec04fa7c20c0956f6d499
cankocak <can_kocak1@epam.com>
2026-02-27 16:19:59 +0300

    completed error handling - stderr and exit

--- chapter_7/error_handling.c
+++ chapter_7/error_handling.c
@@ -11,4 +11,73 @@
  * assigned to a program in the same way that stdin and stdout are. Output
  * written on stderr normally appears on the screen even if the standard output
  * is redirected.
+ *
+ * Let us revise cat to write its error messages on the standard error.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* cat: concatenate files, version 2 */
+int main(int argc, char *argv[]) {
+  FILE *fp;
+  void filecopy(FILE *, FILE *);
+  char *prog = argv[0]; /* program name for errors */
+
+  if (argc == 1) /* no args: copy standard input */
+    filecopy(stdin, stdout);
+  else
+    while (--argc > 0)
+      if ((fp = fopen(*++argv, "r")) == NULL) {
+        fprintf(stderr, "%s: can't open %s\n", prog, *argv);
+        exit(1);
+      } else {
+        filecopy(fp, stdout);
+        fclose(fp);
+      }
+  if (ferror(stdout)) {
+    fprintf(stderr, "%s: error writing stdout\n", prog);
+    exit(2);
+  }
+  exit(0);
+}
+
+void filecopy(FILE *ifp, FILE *ofp) {
+  int c;
+
+  while ((c = getc(ifp)) != EOF)
+    putc(c, ofp);
+}
+
+/***
+ * The program signals errors in two ways, First, the diagnostic output produced
+ * by fprintf goes to stderr, so it finds its way to the screen instead of
+ * disappearing down a pipeline or into an output file. We included the program
+ * name, from argv[0], in the message, so if this program is used with others,
+ * the source of an error is identified.
+ *
+ * Second, the program uses the standard library function exit, which terminates
+ * program execution when it is called. The argument of exit is available to
+ * whatever process called this one, so the success or failure of the program
+ * can be tested by another program that uses this one as a sub-process.
+ * Conventionally, a return value of 0 signals that all is well; non-zero values
+ * usually signal abnormal situations. exit calls fclose for each open output
+ * file, to flush out any buffered output.
+ *
+ * Within main, return expr is equivalent to exit(expr). exit has the advantage
+ * that it can be called from other functions, and that calls to it can be found
+ * with a pattern-searching program.
+ *
+ * The function ferror returns non-zero if an error occurred on the stream fp.
+ *
+ *  int ferror(FILE *fp)
+ * Although output errors are rare, they do occur (for example, if a disk fills
+ * up), so a production program should check this as well.
+ *
+ * The function feof(FILE *) is analogous to ferror; it returns non-zero if end
+ * of file has occurred on the specified file.
+ *
+ *  int feof(FILE *fp)
+ * We have generally not worried about exit status in our small illustrative
+ * prgorams, but any serious program should take care to return sensible, useful
+ * status values.
  */
