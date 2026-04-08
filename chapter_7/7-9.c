b83c5ffc09d4a9a580763e2318afb01308d2888d
cankocak <can_kocak1@epam.com>
2026-04-08 18:25:39 +0300

    completed chapter 7

--- chapter_7/7-9.c
+++ chapter_7/7-9.c
@@ -1,3 +1,16 @@
 /* Functions like isupper can be implemented to save space or to save time.
  * Explore both possibilities.
  */
+#include <stdio.h>
+#include <string.h>
+
+#define ISUPPER(c) ((((c) >= 65) && ((c) <= 90)) ? 1 : 0)
+
+int _isupper(int c) { return (c >= 65 && c <= 90) ? 1 : 0; }
+
+int main(void) {
+  const char *name = "Can Kocak";
+
+  for (size_t i = 0; i < strlen(name); i++)
+    printf("Result for %c: %d\n", name[i], ISUPPER(name[i]));
+}
