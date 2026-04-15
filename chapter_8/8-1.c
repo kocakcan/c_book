5f756d9fd68aba26720c5dca71f92190b31d62f6
cankocak <can_kocak1@epam.com>
2026-04-15 19:11:06 +0300

    completed cat with syscalls

--- chapter_8/8-1.c
+++ chapter_8/8-1.c
@@ -3,3 +3,38 @@
  * instead of their standard library equivalents. Perform experiments to
  * determine the relative speeds of the two versions.
  */
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void error(char *fmt, ...);
+
+/* cat: concatenate files - read / write / open / close */
+int main(int argc, char *argv[]) {
+  int fd;
+  void filecopy(int ifd, int ofd);
+
+  if (argc == 1) /* no args; copy standard input */
+    filecopy(0, 1);
+  else
+    while (--argc > 0)
+      if ((fd = open(*++argv, O_RDONLY, 0)) == -1) {
+        printf("cat: can't open %s\n", *argv);
+        return 1;
+      } else {
+        filecopy(fd, 1);
+        close(fd);
+      }
+
+  return 0;
+}
+
+/* filecopy: copy file ifd to file ofd */
+void filecopy(int ifd, int ofd) {
+  int n;
+  char buf[BUFSIZ];
+
+  while ((n = read(ifd, buf, BUFSIZ)) > 0)
+    if (write(ofd, buf, n) != n)
+      error("cat: write error");
+}
