e91bda236c6435881c8ad936e12e683bc22e5ead
cankocak <can_kocak1@epam.com>
2026-05-04 23:05:02 +0300

    that's what I call programming

--- chapter_8/example_fopen_gatc.c
+++ chapter_8/example_fopen_gatc.c
@@ -78,3 +78,51 @@ int _flushbuf(int, FILE *);
  * flag bits to indicate the proper state. fopen does not allocate any buffer
  * space; this is done by _fillbuf when the file is first read.
  */
+#include <fcntl.h>
+#include <unistd.h>
+#define PERMS 0666 /* RW for owners, group, others */
+
+FILE *_fopen(char *name, char *mode) {
+  int fd;
+  FILE *fp;
+
+  if (*mode != 'r' && *mode != 'w' && *mode != 'a')
+    return NULL;
+  for (fp = _iob; fp < _iob + OPEN_MAX; fp++)
+    if ((fp->flag & (_READ | _WRITE)) == 0)
+      break;                 /* found free slot */
+  if (fp >= _iob + OPEN_MAX) /* no free slots */
+    return NULL;
+  if (*mode == 'w')
+    fd = creat(name, PERMS);
+  else if (*mode == 'a') {
+    if ((fd = open(name, O_WRONLY, 0)) == -1)
+      fd = creat(name, PERMS);
+    lseek(fd, 0L, 2);
+  } else
+    fd = open(name, O_RDONLY, 0);
+  if (fd == -1) /* couldn't access name */
+    return NULL;
+  fp->fd = fd;
+  fp->cnt = 0;
+  fp->base = NULL;
+  fp->flag = (*mode == 'r') ? _READ : _WRITE;
+  return fp;
+}
+
+/***
+ * This version of fopen does not handle all of the access mode possibilities of
+ * the standard, though adding them would not take much code. In particular, our
+ * fopen does not recognize the "b" that signals binary access, since that is
+ * meaningless on UNIX systems, nor the "+" that permits both reading and
+ * writing.
+ *
+ * The first call to getc for a particular file finds a count of zero, which
+ * forces a call of _fillbuf. If _fillbuf finds that file is not open for
+ * reading, it returns EOF immediately. Otherwise, it tries to allocate a buffer
+ * (if reading is to be buffered).
+ *
+ * Once the buffer is established, _fillbuf calls read to fill it, sets the
+ * count and pointers, and returns the character at the beginning of the buffer.
+ * Subsequent calls to _fillbuf will find a buffer allocated.
+ */
